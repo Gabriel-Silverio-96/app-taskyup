414bf3814395c1134eb3075caec1e3abe5146066
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-07-17 11:08:00 -0300

    feat(logo): add logo component

---
+++ src/common/components/Logo/Logo.tsx
@@ -0,0 +1,13 @@
+import { useThemeContext } from "common/context/ThemeContext";
+import React from "react";
+import LogoView from "./LogoView";
+import { ILogo } from "./types/Logo.component";
+
+const Logo: React.FC<ILogo> = ({ size = 143.146, isoType }) => {
+	const { themeName, theme } = useThemeContext();
+	const logoColor = themeName === "dark" ? theme.palette.common.white : theme.palette.common.black;
+
+	return <LogoView {...{ logoColor, size, isoType }} />;
+};
+
+export default Logo;
---
+++ src/common/components/Logo/types/Logo.component.ts
@@ -0,0 +1,8 @@
+export interface ILogo {
+    size?: number;
+    isoType?: boolean;
+}
+
+export interface ILogoView extends ILogo {
+    logoColor?: string;    
+}
