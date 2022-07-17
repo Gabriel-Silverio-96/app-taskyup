d53e13f3d68e3cb1fb5c1302c88bfa7e9736dd3b
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-07-16 23:39:26 -0300

    feat(index): add css baseline

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "./common/context/ThemeContext";
 import ReactDOM from "react-dom/client";
 import MainRoutes from "./MainRoutes";
+import { CssBaseline } from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
 	<ThemeProvider>
+		<CssBaseline />
 		<MainRoutes />
 	</ThemeProvider>
 );
