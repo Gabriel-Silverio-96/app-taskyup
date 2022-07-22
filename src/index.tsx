39c7d1d84afa56119543b3d1d8d56c768d92be2a
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-07-22 20:37:27 -0300

    feat(index): import layout

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { CssBaseline } from "@mui/material";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "shared/common/store";
+import Layout from "shared/components/Layout";
 import MainRoutes from "./MainRoutes";
 import { ThemeProvider } from "./shared/common/context/ThemeContext";
 
@@ -10,8 +11,10 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
 	<Provider store={store}>
 		<ThemeProvider>
-			<CssBaseline />
-			<MainRoutes />
+			<Layout>
+				<CssBaseline />
+				<MainRoutes />
+			</Layout>
 		</ThemeProvider>
 	</Provider>
 );
