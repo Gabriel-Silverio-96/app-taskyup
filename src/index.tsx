f23fa78a0f685aa6f84dae34effb5d85830dcaec
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-07-23 20:53:54 -0300

    feat(index): config interceptors

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import { CssBaseline } from "@mui/material";
+import { createBrowserHistory } from "history";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "shared/common/store";
 import Layout from "shared/components/Layout";
+import setupInterceptors from "shared/services/setupInterceptors";
 import MainRoutes from "./MainRoutes";
 import { ThemeProvider } from "./shared/common/context/ThemeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const history = createBrowserHistory();
+setupInterceptors(history, store);
 
 root.render(
 	<Provider store={store}>
