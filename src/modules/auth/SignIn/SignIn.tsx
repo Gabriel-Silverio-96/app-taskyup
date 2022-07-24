32ef0f668385794f231fb6307317f62b3539cb60
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-07-24 13:06:07 -0300

    feat(signin): dispatch isAuthenticated

--- src/modules/auth/SignIn/SignIn.tsx
+++ src/modules/auth/SignIn/SignIn.tsx
@@ -2,18 +2,22 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { AxiosResponse } from "axios";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { SIGNIN_TYPE } from "shared/common/store/Auth/Auth.reducer";
+import { createAction } from "shared/common/store/store.action";
 import api from "shared/services/api";
 import schema from "./shared/schema";
 import { ISignInForm } from "./shared/schema/types/types.schema";
 import SignInView from "./SignInView";
 import { IDataUser, IFetchSignInResponse } from "./types/SignIn.component";
-import { useNavigate } from "react-router-dom";
 
 const SignIn: React.FC = () => {
 	const { register, handleSubmit, formState: { errors } }
 		= useForm<ISignInForm>({ resolver: yupResolver(schema), mode: "all" });
 
 	const navigate = useNavigate();
+	const dispatch = useDispatch();
 	const [isLoading, setIsLoading] = useState(false);
 	const [showPassword, setShowPassword] = useState(false);
 	const handleShowPassword = () => setShowPassword(prevState => !prevState);
@@ -21,9 +25,10 @@ const SignIn: React.FC = () => {
 	const fetchSignIn = async (dataUser: IDataUser) => {
 		setIsLoading(true);
 		try {
-			const { data } = await api.post("auth/login", dataUser) as AxiosResponse<IFetchSignInResponse>;
-			localStorage.setItem("@taskyup.token", data.token);
-			localStorage.setItem("@taskyup.user_data", JSON.stringify(data.user_data));
+			const { data: { token, user_data } } = await api.post("auth/login", dataUser) as AxiosResponse<IFetchSignInResponse>;
+			localStorage.setItem("@taskyup.token", token);
+			localStorage.setItem("@taskyup.user_data", JSON.stringify(user_data));
+			dispatch(createAction(SIGNIN_TYPE, { isAuthenticated: true, user_data }));
 			return navigate("/board");
 		} catch (error) {
 			console.error("SignIn ", error);
