bdbe0ff4a1c209f90115fb002bfdf877c0fac009
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-07-24 13:07:56 -0300

    feat(components): add useSelector

--- src/shared/components/RequiredAuth/RequiredAuth.tsx
+++ src/shared/components/RequiredAuth/RequiredAuth.tsx
@@ -1,9 +1,11 @@
+import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { IAuthState } from "shared/common/store/Auth/types/Auth.types";
 import { IRequiredAuth } from "./types/RequiredAuth.component";
 
 const RequiredAuth: React.FC<IRequiredAuth> = ({ children }) => {
-	const authenticated = false;
-	return authenticated ? children : <Navigate to="/auth/signin" />;
+	const { isAuthenticated } = useSelector((state: { auth: IAuthState }) => state.auth);
+	return isAuthenticated ? children : <Navigate to="/auth/signin" />;
 };
 
 export default RequiredAuth;
