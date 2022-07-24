35c604c42cdaadf933084dd5628e3ebdb90e1c92
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-07-24 13:07:25 -0300

    fix(components): type SnackBar

--- src/shared/components/Layout/SnackBar/SnackBar.tsx
+++ src/shared/components/Layout/SnackBar/SnackBar.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import useSnackBar from "shared/common/hook/useSnackBar";
-import { ISnackbarAction } from "shared/common/store/SnackBar/types/Snackbar.types";
+import { ISnackBarState } from "shared/common/store/SnackBar/types/Snackbar.types";
 import SnackBarView from "./SnackBarView";
 
 const SnackBar: React.FC = () => {
 	const { snackBarClose } = useSnackBar();
-	const snackBarProps = useSelector((state: { snackBar: ISnackbarAction }) => state.snackBar);	
+	const snackBarProps = useSelector((state: { snackBar: ISnackBarState }) => state.snackBar);	
 
 	return <SnackBarView {...{ snackBarProps, snackBarClose }} />;
 };
