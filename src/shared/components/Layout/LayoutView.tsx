a9721263f4c1a48784448c8339814d35b889ebb1
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-07-26 21:47:05 -0300

    feat(layout): import header

--- src/shared/components/Layout/LayoutView.tsx
+++ src/shared/components/Layout/LayoutView.tsx
@@ -1,12 +1,16 @@
+import Header from "./Header";
 import SnackBar from "./SnackBar";
 import { TLayoutView } from "./types/Layout.component";
 
 const LayoutView: React.FC<TLayoutView> = ({ children }) => {
 	return (
-		<main>
-			<SnackBar />
-			{children}
-		</main>
+		<>
+			<Header />
+			<main>
+				<SnackBar />
+				{children}
+			</main>
+		</>
 	);
 };
 
