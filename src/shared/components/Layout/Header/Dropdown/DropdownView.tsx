b1d791021cc5950887bff5690f61a8931550b824
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-07-26 21:46:47 -0300

    feat(layout): create dropdown component

---
+++ src/shared/components/Layout/Header/Dropdown/DropdownView.tsx
@@ -0,0 +1,31 @@
+import { List, ListItem, ListItemText } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Dropdown } from "./style";
+
+const DropdownView: React.FC<any> = ({ children, open }) => {
+	return (
+		<Dropdown>
+			{children}
+			{ open && (
+				<List>
+					<ListItem disablePadding>
+						<Link to="/profile">
+							<ListItemText primary="Profile" />
+						</Link>
+					</ListItem>
+					<ListItem disablePadding>
+						<Link to="/setting">
+						    <ListItemText primary="Setting" />
+						</Link>
+					</ListItem>
+					<ListItem disablePadding>
+						<ListItemText primary="Exit" />
+					</ListItem>
+				</List>
+			)}
+		</Dropdown>
+	);
+};
+
+export default DropdownView;
