c21cfdddcbd395494724b0446231868e8ff3e328
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-07-27 21:39:24 -0300

    feat(required-auth): import component Drawer

--- src/shared/components/RequiredAuth/RequiredAuth.tsx
+++ src/shared/components/RequiredAuth/RequiredAuth.tsx
@@ -2,11 +2,20 @@ import { memo } from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { IAuthState } from "shared/common/store/Auth/types/Auth.types";
+import Drawer from "../Drawer";
 import { IRequiredAuth } from "./types/RequiredAuth.component";
 
 const RequiredAuth: React.FC<IRequiredAuth> = ({ children }) => {
 	const { isAuthenticated } = useSelector((state: { auth: IAuthState }) => state.auth);
-	return isAuthenticated ? children : <Navigate to="/auth/signin" />;
+	return (
+		<>
+			{isAuthenticated ? (
+				<Drawer>
+					{children}
+				</Drawer>
+			) : <Navigate to="/auth/signin" />}
+		</>
+	);
 };
 
 export default memo(RequiredAuth);
