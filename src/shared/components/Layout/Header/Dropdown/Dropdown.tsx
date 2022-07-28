8ac3553ffa2768eb43dd6fe37cf5daca1e6cb6dc
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-07-27 21:40:17 -0300

    refactor(layout): remove header

--- src/shared/components/Layout/Header/Dropdown/Dropdown.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import DropdownView from "./DropdownView";
-
-const Dropdown: React.FC<any> = ({ children, open }) => {
-	return <DropdownView {... { children, open }} />;
-};
-
-export default Dropdown;
