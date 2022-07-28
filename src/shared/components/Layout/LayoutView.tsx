b15c4e56baa076040367b8104855d56bd76b68e7
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-07-27 21:42:01 -0300

    refactor(layout): remove header

--- src/shared/components/Layout/LayoutView.tsx
+++ src/shared/components/Layout/LayoutView.tsx
@@ -1,16 +1,12 @@
-import Header from "./Header";
 import SnackBar from "./SnackBar";
 import { TLayoutView } from "./types/Layout.component";
 
 const LayoutView: React.FC<TLayoutView> = ({ children }) => {
 	return (
-		<>
-			<Header />
-			<main>
-				<SnackBar />
-				{children}
-			</main>
-		</>
+		<main>
+			<SnackBar />
+			{children}
+		</main>
 	);
 };
 
