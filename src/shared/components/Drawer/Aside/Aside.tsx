b510150baf5c52d15699440b72787e7536d02cfc
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2022-07-29 20:45:53 -0300

    feat(aside): import useLocalStorage

--- src/shared/components/Drawer/Aside/Aside.tsx
+++ src/shared/components/Drawer/Aside/Aside.tsx
@@ -1,8 +1,9 @@
-import React, { memo, useState } from "react";
+import React, { memo } from "react";
+import useLocalStorage from "shared/common/hook/useLocalStorage";
 import AsideView from "./AsideView";
 
 const Aside: React.FC = () => {
-	const [openAside, setOpenAside] = useState(true);
+	const [openAside, setOpenAside] = useLocalStorage("@taskyup.aside.open", true);
 	const toogleOpenAside = () => setOpenAside(prevState => !prevState);
 	
 	return <AsideView {...{ openAside, toogleOpenAside }} />;
