f56d43abbef70b37db61cd988b4143dc58ba8fbb
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-08-05 20:57:33 -0300

    feat(main-index): import react-query

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { CssBaseline } from "@mui/material";
 import { createBrowserHistory } from "history";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "shared/common/store";
@@ -13,18 +14,21 @@ import { ThemeProvider } from "./shared/common/context/ThemeContext";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 const history = createBrowserHistory();
 setupInterceptors(history, store);
+const queryClient = new QueryClient();
 
 root.render(
 	<Provider store={store}>
-		<ThemeProvider>
-			<BrowserRouter>
-				<Session>
-					<Layout>
-						<CssBaseline />
-						<MainRoutes />
-					</Layout>
-				</Session>
-			</BrowserRouter>
-		</ThemeProvider>
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider>
+				<BrowserRouter>
+					<Session>
+						<Layout>
+							<CssBaseline />
+							<MainRoutes />
+						</Layout>
+					</Session>
+				</BrowserRouter>
+			</ThemeProvider>
+		</QueryClientProvider>
 	</Provider>
 );
