415d9d4d63b02d5b938f223572a3eb6b345f88a9
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-08-05 20:59:30 -0300

    feat(aside): request create new board

--- src/shared/components/Drawer/DialogNewBoard/DialogNewBoard.tsx
+++ src/shared/components/Drawer/DialogNewBoard/DialogNewBoard.tsx
@@ -2,22 +2,30 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { useMediaQuery, useTheme } from "@mui/material";
 import React, { memo, useMemo } from "react";
 import { useForm } from "react-hook-form";
+import api from "shared/services/api";
 import DialogNewBoardView from "./DialogNewBoardView";
 import schema from "./shared/schema";
+import { useQueryClient } from "@tanstack/react-query";
 
 const DialogNewBoard: React.FC<any> = ({ openDialog, closeDialogNewBoard }) => {
 	const theme = useTheme();
+	const queryClient = useQueryClient();
 	const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
-
 	const { register, handleSubmit,	formState: { errors }, reset }  
 		= useForm({ resolver: yupResolver(schema), mode: "all" });	
-
-	const fetchDialogNewBoard = (dataNewBoard: any) => {
-		console.log(dataNewBoard);
-	};
-
+		
 	useMemo(() => !openDialog && reset(), [openDialog]);
-	
+
+	const fetchDialogNewBoard = async (dataNewBoard: any) => {
+		try {	
+			await api.post("board/create", dataNewBoard) as any;
+			queryClient.invalidateQueries(["board"]);
+			closeDialogNewBoard();
+		} catch (error) {
+			console.error("DialogNewBoard ", error);			
+		}
+	};
+		
 	return (
 		<DialogNewBoardView
 			{...{
--- src/shared/components/Drawer/DialogNewBoard/DialogNewBoardView.tsx
+++ src/shared/components/Drawer/DialogNewBoard/DialogNewBoardView.tsx
@@ -65,7 +65,7 @@ const DialogNewBoardView: React.FC<any> = (props) => {
 								error={errors.board_type_id && Boolean(errors.board_type_id)}
 								helperText={errors.board_type_id ? errors.board_type_id?.message : ""}
 							>
-								<MenuItem value="739ddd82-c403-49be-96da-bac53f0f6dae">Notes</MenuItem>
+								<MenuItem value="1f819d7d-11b5-4742-be86-2286ef50b5d5">Notes</MenuItem>
 								<MenuItem value="7b784aee-0133-4d6a-b14b-8dbbe19e9428" disabled>Text</MenuItem>
 								<MenuItem value="1cf4ff82-59bd-4733-890c-c9226ea0df2f" disabled>Kanban</MenuItem>
 							</TextField>
