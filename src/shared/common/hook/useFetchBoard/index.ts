7b158944c999ca063e3c0885ce4d10ae74f751db
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-08-05 20:58:02 -0300

    feat(hook): create useFetchBoard

---
+++ src/shared/common/hook/useFetchBoard/index.ts
@@ -0,0 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
+import api from "shared/services/api";
+import { IFetchBoard, IUseFetchBoard } from "./types/UseFetchBoard.types";
+
+const useFetchBoard = (): IUseFetchBoard => {
+	const fetchBoard = async () => {
+		const { data } = await api.get("/board") as AxiosResponse<IFetchBoard>;
+		return data;
+	};
+	const { isError, isLoading, data: board } = useQuery(["board"], fetchBoard);
+	return { isError, isLoading, board };
+};
+
+export default useFetchBoard;
---
+++ src/shared/common/hook/useFetchBoard/types/UseFetchBoard.types.ts
@@ -0,0 +1,13 @@
+export interface IFetchBoard {
+	board_id: string;
+	title: string;
+	type_board: string;
+	created_at: string;
+	members_board: string[];
+}
+
+export interface IUseFetchBoard {
+    isError: boolean;
+    isLoading: boolean;
+    board: IFetchBoard | undefined;
+}
