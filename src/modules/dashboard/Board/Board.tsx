b05bc867f8060e2cd49f25b94ba075f40f2b35e7
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-08-08 20:40:45 -0300

    feat(board): import context

--- src/modules/dashboard/Board/Board.tsx
+++ src/modules/dashboard/Board/Board.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import BoardView from "./BoardView";
+import { ContextProviderBoard } from "./Context";
 
 const Board: React.FC = () => {	
-	return <BoardView />;
+	return (
+		<ContextProviderBoard>
+			<BoardView />
+		</ContextProviderBoard>
+	);
 };
 
 export default Board;
