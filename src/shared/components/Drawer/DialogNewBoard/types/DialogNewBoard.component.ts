d0ccad0fad881798c6a90fb569e7dd5754e237cb
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-08-09 20:33:40 -0300

    feat(dialog-new-board): add types

---
+++ src/shared/components/Drawer/DialogNewBoard/types/DialogNewBoard.component.ts
@@ -0,0 +1,20 @@
+import { FieldErrors, UseFormHandleSubmit, UseFormRegister } from "react-hook-form";
+
+export interface IDialogNewBoard {
+    openDialog: boolean;
+    closeDialogNewBoard: () => void;
+}
+
+export interface IDialogNewBoardForm {
+    title: string;
+    board_type_id: string;
+}
+
+export interface IDialogNewBoardView extends IDialogNewBoard {
+    register: UseFormRegister<any>;
+    fullScreen: boolean;    
+    handleSubmit: UseFormHandleSubmit<any>;
+    fetchDialogNewBoard: (dataNewBoard: IDialogNewBoardForm) => void;
+    errors: FieldErrors<IDialogNewBoardForm>;
+    isLoading: boolean;
+}
