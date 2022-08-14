8855fc73c8b7deb6c2d09f232279af25e84204cc
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-08-14 15:01:00 -0300

    feat(board): import context and hook to dialog delete single board

--- src/modules/dashboard/Board/DialogDeleteSingleBoard/DialogDeleteSingleBoard.tsx
+++ src/modules/dashboard/Board/DialogDeleteSingleBoard/DialogDeleteSingleBoard.tsx
@@ -1,8 +1,17 @@
 import React, { memo } from "react";
+import { useContextBoard } from "../Context";
+import useDialogBoard from "../shared/hook/useDialogBoard";
 import DialogDeleteSingleBoardView from "./DialogDeleteSingleBoardView";
 
 const DialogDeleteSingleBoard: React.FC = () => {
-	return <DialogDeleteSingleBoardView />;
+	const { isOpenDialogDeleteSingleBoard, boardID } = useContextBoard();
+	const { closeDialogDeleteSingleBoard } = useDialogBoard();
+    
+	return (
+		<DialogDeleteSingleBoardView
+			{...{ isOpenDialogDeleteSingleBoard, closeDialogDeleteSingleBoard }}
+		/>
+	);
 };
 
 export default memo(DialogDeleteSingleBoard);
--- src/modules/dashboard/Board/DialogDeleteSingleBoard/DialogDeleteSingleBoardView.tsx
+++ src/modules/dashboard/Board/DialogDeleteSingleBoard/DialogDeleteSingleBoardView.tsx
@@ -1,8 +1,59 @@
+import { LoadingButton } from "@mui/lab";
+import {
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogTitle, Grid,
+	IconButton,
+	Typography
+} from "@mui/material";
 import React from "react";
+import { MdOutlineClose } from "react-icons/md";
 
-const DialogDeleteSingleBoardView: React.FC<any> = () => {
+const DialogDeleteSingleBoardView: React.FC<any> = (props) => {
+	const { isOpenDialogDeleteSingleBoard, closeDialogDeleteSingleBoard } = props;
 	return (
-		<h1>DialogDeleteSingleBoardView</h1>
+		<Dialog 
+			fullWidth
+			maxWidth="xs"
+			open={isOpenDialogDeleteSingleBoard}			
+			onClose={closeDialogDeleteSingleBoard}
+		>
+			<DialogTitle sx={{marginBottom: "1rem"}}>
+				<Grid container alignItems="center" justifyContent="space-between">
+					<Grid item>
+						<Typography variant="h6" fontWeight={700}>Delete Board</Typography>
+					</Grid>
+					<Grid item> 
+						<IconButton onClick={closeDialogDeleteSingleBoard}>
+							<MdOutlineClose />
+						</IconButton>
+					</Grid>
+				</Grid>
+			</DialogTitle>
+			<DialogContent>
+				<Typography color="GrayText">
+                    Would you like to delete board? This action deletes all notes from this board
+				</Typography>
+			</DialogContent>
+			<DialogActions>
+				<Grid container justifyContent="space-between" alignItems="center">
+					<Grid item md="auto">
+						<Button variant="outlined" onClick={closeDialogDeleteSingleBoard}>No</Button>
+					</Grid>
+					<Grid item md="auto">
+						<LoadingButton 							
+							form="form-new-board"
+							variant="contained"
+							type="submit"
+						>
+                            Yes
+						</LoadingButton>				
+					</Grid>
+				</Grid>
+			</DialogActions>		
+		</Dialog>
 	);
 };
 
