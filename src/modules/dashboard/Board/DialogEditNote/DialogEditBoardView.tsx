9e58d5268f03a54050389e55c353229e99359cee
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-08-13 22:10:14 -0300

    refactor(dialog-edit-board): loading

--- src/modules/dashboard/Board/DialogEditNote/DialogEditBoardView.tsx
+++ src/modules/dashboard/Board/DialogEditNote/DialogEditBoardView.tsx
@@ -10,6 +10,7 @@ import {
 import React from "react";
 import { MdOutlineClose } from "react-icons/md";
 import Date from "shared/components/Date";
+import Loading from "shared/components/Loading";
 
 const DialogEditBoardView: React.FC<any> = (props) => {
 	const { 
@@ -23,13 +24,16 @@ const DialogEditBoardView: React.FC<any> = (props) => {
 		isOpenDialogEditBoard,
 		closeDialogEditBoard,
 	} = props;
+
+	const loadingGrid = { display: isLoading ? "flex" : "none", mb: 5 };
+
 	return (
 		<Dialog 
 			fullWidth
 			maxWidth="xs"
 			open={isOpenDialogEditBoard}
 			fullScreen={fullScreen}
-			onClose={!isSaving ? closeDialogEditBoard : () => ""}
+			onClose={!isSaving && !isLoading ? closeDialogEditBoard : () => ""}
 		>
 			<DialogTitle sx={{marginBottom: "1rem"}}>
 				<Grid container alignItems="center" justifyContent="space-between">
@@ -37,50 +41,62 @@ const DialogEditBoardView: React.FC<any> = (props) => {
 						<Typography variant="h6" fontWeight={700}>Edit Board</Typography>
 					</Grid>
 					<Grid item> 
-						<IconButton onClick={closeDialogEditBoard} disabled={isSaving}>
+						<IconButton onClick={closeDialogEditBoard} disabled={isLoading || isSaving}>
 							<MdOutlineClose />
 						</IconButton>
 					</Grid>
 				</Grid>
 			</DialogTitle>
-			<DialogContent>
-				<form 
-					id="form-new-board"
-					onSubmit={handleSubmit(fetchDialogEditBoard)}>
-					<Grid container direction="column" spacing={5}>						
-						<Grid item>
-							<TextField 
-								label="Board name"
-								size="small"
-								fullWidth
-								{...register("title")}
-								error={errors.title && Boolean(errors.title)}
-								helperText={errors.title ? errors.title?.message : ""}
-							/>
+			<Grid container justifyContent="center" sx={loadingGrid}>
+				<Loading isLoading={isLoading} message="Loading" />
+			</Grid>
+			{!isLoading && (
+				<>
+					<DialogContent>
+						<form 
+							id="form-new-board"
+							onSubmit={handleSubmit(fetchDialogEditBoard)}>
+							<Grid container direction="column" spacing={5}>						
+								<Grid item>
+									<TextField 
+										label="Board name"
+										size="small"
+										fullWidth
+										{...register("title")}
+										error={errors.title && Boolean(errors.title)}
+										helperText={errors.title ? errors.title?.message : ""}
+									/>
+								</Grid>
+							</Grid>
+						</form>
+					</DialogContent>
+					<DialogActions>
+						<Grid container justifyContent="space-between" alignItems="center">
+							<Grid item md="auto">
+								<Typography 
+									variant="caption" 
+									color="GrayText" 
+									display="flex" 
+									alignItems="center" sx={{ gap: 0.5 } }
+								>
+									Created at
+									<Date {...register("created_at")} />
+								</Typography>
+							</Grid>
+							<Grid item md="auto">
+								<LoadingButton 							
+									form="form-new-board"
+									variant="contained"
+									type="submit"
+									loading={isSaving}
+								>
+									Save
+								</LoadingButton>				
+							</Grid>
 						</Grid>
-					</Grid>
-				</form>
-			</DialogContent>
-			<DialogActions>
-				<Grid container justifyContent="space-between" alignItems="center">
-					<Grid item md="auto">
-						<Typography variant="caption" color="GrayText" display="flex" alignItems="center" sx={{ gap: 0.5 } }>
-							Created at
-							<Date {...register("created_at")} />
-						</Typography>
-					</Grid>
-					<Grid item md="auto">
-						<LoadingButton 							
-							form="form-new-board"
-							variant="contained"
-							type="submit"
-							loading={isSaving}
-						>
-							Save
-						</LoadingButton>				
-					</Grid>
-				</Grid>
-			</DialogActions>
+					</DialogActions>
+				</>
+			)}
 		</Dialog>
 	);
 };
