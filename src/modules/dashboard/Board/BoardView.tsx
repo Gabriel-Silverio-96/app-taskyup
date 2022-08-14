55a9192270e06848816945da7e6e6b6529b97209
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-08-14 14:25:16 -0300

    refactor(board): import DialogEditBoard

--- src/modules/dashboard/Board/BoardView.tsx
+++ src/modules/dashboard/Board/BoardView.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import React from "react";
 import CardBoard from "./CardBoard";
-import DialogEditBoard from "./DialogEditNote";
+import DialogEditBoard from "./DialogEditBoard";
 
 const BoardView: React.FC = () => {
 	return (
