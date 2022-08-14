00fc6f627dd886404e68911406549472c22a3121
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-08-14 14:31:17 -0300

    refactor(board): add DialogDeleteSingleBoard

---
+++ src/modules/dashboard/Board/DialogDeleteSingleBoard/DialogDeleteSingleBoard.tsx
@@ -0,0 +1,8 @@
+import React, { memo } from "react";
+import DialogDeleteSingleBoardView from "./DialogDeleteSingleBoardView";
+
+const DialogDeleteSingleBoard: React.FC = () => {
+	return <DialogDeleteSingleBoardView />;
+};
+
+export default memo(DialogDeleteSingleBoard);
---
+++ src/modules/dashboard/Board/DialogDeleteSingleBoard/DialogDeleteSingleBoardView.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const DialogDeleteSingleBoardView: React.FC<any> = () => {
+	return (
+		<h1>DialogDeleteSingleBoardView</h1>
+	);
+};
+
+export default DialogDeleteSingleBoardView;
