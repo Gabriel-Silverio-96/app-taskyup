da5426176f98d7a348420515dc600df8991d081a
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-08-18 17:56:23 -0300

    feat(notes): create page

---
+++ src/modules/notes/Note/Note.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import NoteView from "./NoteView";
+
+const Note: React.FC = () => {
+	return <NoteView />;
+};
+
+export default Note;
---
+++ src/modules/notes/Note/NoteView.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+const NoteView: React.FC<any> = () => {
+	return (
+		<Box component="section" sx={{ flexGrow: 1, p: 3 }}>
+			<h1>Page</h1>
+		</Box>	
+	);
+};
+
+export default NoteView;
