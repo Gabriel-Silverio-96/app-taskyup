68f191bf6faca10c3c7de1fb3e4808210b1ccb83
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-08-19 18:46:25 -0300

    feat(common): interface IFetchSingleBoard

--- src/shared/common/types/Fetch/index.ts
+++ src/shared/common/types/Fetch/index.ts
@@ -2,3 +2,9 @@ export interface IFetchResponseDefault {
     type_message: string;
     message: string;
 }
+
+export interface IFetchSingleBoard {
+    title: string;
+    board_type_id: string;
+    created_at: string;
+}
