4e18af7b6a37a5a8613b08f6915165dc145b3bd7
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-08-19 21:13:39 -0300

    feat(notes): import CardNote

--- src/modules/notes/Note/NoteView.tsx
+++ src/modules/notes/Note/NoteView.tsx
@@ -1,11 +1,13 @@
 import { Box } from "@mui/material";
 import React from "react";
+import CardNote from "./CardNote";
 import HeaderNote from "./HeaderNote";
 
 const NoteView: React.FC<any> = () => {
 	return (
 		<Box component="section" sx={{ flexGrow: 1, p: 3 }}>
 			<HeaderNote />
+			<CardNote />
 		</Box>	
 	);
 };
