66decf02efbdb3b24baaec89c59aa2a3d7bacde6
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-08-27 13:38:23 -0300

    feat(DialogEditNote): create component

---
+++ src/modules/notes/Note/DialogEditNote/DialogEditNote.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DialogEditNoteView from "./DialogEditNoteView";
+
+const DialogEditNote: React.FC = () => {
+	return <DialogEditNoteView />;
+};
+
+export default DialogEditNote;
---
+++ src/modules/notes/Note/DialogEditNote/DialogEditNoteView.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const DialogEditNoteView: React.FC<any> = (props) => {
+	return (
+		<h1>Dialog</h1>
+	);
+};
+
+export default DialogEditNoteView;
