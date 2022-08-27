90447f0aae0dcf6adc59b08d3640611a1d73f8be
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-08-27 15:34:14 -0300

    feat(useDialogNote): open and close dialog edit note

--- src/modules/notes/Note/shared/hook/useDialogNote/index.ts
+++ src/modules/notes/Note/shared/hook/useDialogNote/index.ts
@@ -2,14 +2,26 @@ import { useContextNote } from "modules/notes/Note/Context";
 import { IUseDialogNote } from "./types/UseDialogNote";
 
 const useDialogNote = (): IUseDialogNote => {
-	const { setIsOpenDialogNewNote } = useContextNote();
+	const { setIsOpenDialogNewNote, setIsOpenDialogEditNote, setNoteID } = useContextNote();
 
 	const openDialogNewNote = () => setIsOpenDialogNewNote(true);
 	const closeDialogNewNote = () => setIsOpenDialogNewNote(false);
 
+	const openDialogEditNote = (noteID: string) => {
+		setIsOpenDialogEditNote(true);
+		setNoteID(noteID);
+	};
+	
+	const closeDialogEditNote = () => {
+		setIsOpenDialogEditNote(false);
+		setNoteID("");
+	};
+
 	return {
 		openDialogNewNote,
 		closeDialogNewNote,
+		openDialogEditNote,
+		closeDialogEditNote
 	};
 };
 export default useDialogNote;
--- src/modules/notes/Note/shared/hook/useDialogNote/types/UseDialogNote.ts
+++ src/modules/notes/Note/shared/hook/useDialogNote/types/UseDialogNote.ts
@@ -1,4 +1,6 @@
 export interface IUseDialogNote {
     openDialogNewNote: () => void;
     closeDialogNewNote: () => void;
+    openDialogEditNote: (noteID: string) => void;
+    closeDialogEditNote: () => void;
 }
