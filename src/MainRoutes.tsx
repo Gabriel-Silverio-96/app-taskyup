0def6589924ded2edac747a518b1b7087ff9e8ed
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-09-08 20:53:02 -0300

    featMainRoutes): import page ForgotPassword

--- src/MainRoutes.tsx
+++ src/MainRoutes.tsx
@@ -2,6 +2,7 @@ import SignUp from "modules/auth/SignUp";
 import Note from "modules/notes/Note";
 import { Route, Routes } from "react-router-dom";
 import RequiredAuth from "shared/components/RequiredAuth";
+import ForgotPassword from "./modules/auth/ForgotPassword";
 import SignIn from "./modules/auth/SignIn";
 import Board from "./modules/dashboard/Board";
 
@@ -10,6 +11,7 @@ const MainRoutes:React.FC = () => {
 		<Routes>
 			<Route path="/auth/signin" element={<SignIn />} />
 			<Route path="/auth/signup" element={<SignUp />} />
+			<Route path="/auth/forgot-password" element={<ForgotPassword />} />
 
 			<Route path="/dashboard" element={<RequiredAuth><Board /></RequiredAuth>} />
 			<Route path="/notes/:board_id" element={<RequiredAuth><Note /></RequiredAuth>} />				
