af76b86c71c7010a105ca0aeab1a0591f21cb12e
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-09-08 20:53:54 -0300

    refactor(ForgotPassword): types

--- src/modules/auth/ForgotPassword/types/index.ts
+++ src/modules/auth/ForgotPassword/types/index.ts
@@ -1,3 +1,11 @@
+import { FieldErrors, UseFormRegister } from "react-hook-form";
+
 export interface IForgotPasswordForm {
     email: string;
+}
+
+export interface IForgotPasswordView {
+    register: UseFormRegister<IForgotPasswordForm>;
+    errors: FieldErrors<IForgotPasswordForm>
+    onSubmit: () => void;
 }
