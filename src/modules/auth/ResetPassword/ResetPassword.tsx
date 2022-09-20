ffcc3176e70babcaf4f526a219e4ad36a99f006c
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-09-20 18:34:06 -0300

    feat(ResetPassword): create page

---
+++ src/modules/auth/ResetPassword/ResetPassword.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ResetPasswordView from "./ResetPasswordView";
+
+const ResetPassword: React.FC = () => {
+	return <ResetPasswordView />;
+};
+
+export default ResetPassword;
---
+++ src/modules/auth/ResetPassword/ResetPasswordView.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ResetPasswordView: React.FC<any> = () => {    
+	return (
+		<h1>Reset password</h1>
+	);
+};
+
+export default ResetPasswordView;
