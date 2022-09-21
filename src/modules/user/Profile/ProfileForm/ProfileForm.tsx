ed57a383274cee2a0cf8f2a705af1ec52d8dc028
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-09-21 19:16:22 -0300

    feat(ProfileForm): create profile form component

---
+++ src/modules/user/Profile/ProfileForm/ProfileForm.tsx
@@ -0,0 +1,8 @@
+import React, { memo } from "react";
+import ProfileFormView from "./ProfileFormView";
+
+const ProfileForm: React.FC = () => {
+	return <ProfileFormView/>;
+};
+
+export default memo(ProfileForm);
