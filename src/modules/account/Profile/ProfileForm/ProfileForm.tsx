18f30a8bd8f733e247d8583234d75242c4c28060
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-09-22 18:05:28 -0300

    feat(ProfileForm): get profile data

--- src/modules/account/Profile/ProfileForm/ProfileForm.tsx
+++ src/modules/account/Profile/ProfileForm/ProfileForm.tsx
@@ -1,8 +1,26 @@
-import React, { memo } from "react";
+import { AxiosResponse } from "axios";
+import React, { memo, useEffect, useState } from "react";
+import api from "shared/services/api";
 import ProfileFormView from "./ProfileFormView";
+import { IProfileUserData } from "./types";
 
 const ProfileForm: React.FC = () => {
-	return <ProfileFormView/>;
+	const [isLoading, setIsLoading] = useState(true);
+	const [profileUserData, setProfileUserData] = useState({} as IProfileUserData);
+
+	const fetchProfileForm = async () => {
+		try {
+			const { data } = await api.get("auth/account") as AxiosResponse<IProfileUserData>;
+			setProfileUserData(data);			
+		} catch (error) {
+			console.error("ProfileForm ", error);			
+		} finally {
+			setIsLoading(false);
+		}
+	};
+	useEffect(() => {fetchProfileForm();}, []);
+
+	return <ProfileFormView {...{ isLoading, profileUserData }}/>;
 };
 
 export default memo(ProfileForm);
---
+++ src/modules/account/Profile/ProfileForm/types/index.ts
@@ -0,0 +1,4 @@
+export interface IProfileUserData {
+    full_name: string;
+    email: string;
+}
