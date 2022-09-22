d5f14cdd4510e2469bb891da35360eae5c862a37
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-09-22 18:43:17 -0300

    feat(ProfileForm): set profile data in form

--- src/modules/account/Profile/ProfileForm/ProfileForm.tsx
+++ src/modules/account/Profile/ProfileForm/ProfileForm.tsx
@@ -1,26 +1,43 @@
-import { AxiosResponse } from "axios";
-import React, { memo, useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import React, { memo } from "react";
+import { useForm } from "react-hook-form";
 import api from "shared/services/api";
 import ProfileFormView from "./ProfileFormView";
 import { IProfileUserData } from "./types";
 
 const ProfileForm: React.FC = () => {
-	const [isLoading, setIsLoading] = useState(true);
-	const [profileUserData, setProfileUserData] = useState({} as IProfileUserData);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		setValue,
+	} = useForm<IProfileUserData>();
 
 	const fetchProfileForm = async () => {
-		try {
-			const { data } = await api.get("auth/account") as AxiosResponse<IProfileUserData>;
-			setProfileUserData(data);			
-		} catch (error) {
-			console.error("ProfileForm ", error);			
-		} finally {
-			setIsLoading(false);
-		}
+		const { data } = (await api.get("auth/account"));
+		return data;
 	};
-	useEffect(() => {fetchProfileForm();}, []);
 
-	return <ProfileFormView {...{ isLoading, profileUserData }}/>;
+	const onSuccessFetchProfileForm = (data: IProfileUserData) => {
+		setValue("full_name", data.full_name);
+		setValue("email", data.email);
+	};
+
+	const { data: profileUserData, isLoading } = useQuery<IProfileUserData>(
+		["profile_form"],
+		fetchProfileForm,
+		{ onSuccess: onSuccessFetchProfileForm }
+	);
+
+	const fetchSaveProfileForm = (form: IProfileUserData) => console.log(form);
+
+	const onSubmit = handleSubmit(fetchSaveProfileForm);
+
+	return (
+		<ProfileFormView
+			{...{ register, isLoading, profileUserData, onSubmit }}
+		/>
+	);
 };
 
 export default memo(ProfileForm);
--- src/modules/account/Profile/ProfileForm/ProfileFormView.tsx
+++ src/modules/account/Profile/ProfileForm/ProfileFormView.tsx
@@ -1,18 +1,24 @@
 import { LoadingButton } from "@mui/lab";
 import { Grid, TextField, Typography } from "@mui/material";
 import React from "react";
+import Loading from "shared/components/Loading";
 
-const ProfileFormView: React.FC<any> = () => {
+const ProfileFormView: React.FC<any> = (props) => {
+	const { register, isLoading, profileUserData, onSubmit } = props;	
+
 	return (
 		<>
+			<Loading backdrop isLoading={isLoading} />
 			<Typography variant="h5" sx={{ mb: 5 }}>Hi, Camila</Typography>
-			<form>
+			<form method="post" onSubmit={onSubmit}>
 				<Grid container direction="column" spacing={5}>
 					<Grid item>
 						<TextField 
 							label="Full name" 
 							size="small" 
-							fullWidth 															
+							fullWidth
+							defaultValue={profileUserData?.full_name}
+							{...register("full_name")}							
 						/>
 					</Grid>
 					<Grid item>
@@ -21,8 +27,9 @@ const ProfileFormView: React.FC<any> = () => {
 							size="small"
 							disabled
 							aria-disabled="true"
-							value="email@email.com" 
-							fullWidth							
+							fullWidth				
+							defaultValue={profileUserData?.email}
+							{...register("email")}			
 						/>
 					</Grid>
 					<Grid item textAlign="right">
