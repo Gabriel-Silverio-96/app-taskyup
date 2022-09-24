e2ca2dcad62f32c713cb410654acee601e9e7b8a
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-09-24 12:04:32 -0300

    feat(Page404): create page

---
+++ src/modules/error/Page404/Page404.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Page404View from "./Page404View";
+
+const Page404: React.FC = () => {
+	const navigate = useNavigate();    
+	const backPage = () => navigate(-1);
+
+	return <Page404View {...{ backPage }}/>;
+};
+
+export default Page404;
