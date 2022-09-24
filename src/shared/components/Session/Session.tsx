afe27ac44822688478cace9afcf4cea30755ada9
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-09-24 12:22:22 -0300

    feat(Session): redirect to last page accessed

--- src/shared/components/Session/Session.tsx
+++ src/shared/components/Session/Session.tsx
@@ -1,6 +1,9 @@
-import { useLayoutEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect, useLayoutEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
+import useLocalStorage from "shared/common/hook/useLocalStorage";
 import { SIGNIN_TYPE } from "shared/common/store/Auth/Auth.reducer";
+import { IAuthState } from "shared/common/store/Auth/types/Auth.types";
 import { createAction } from "shared/common/store/store.action";
 import api from "shared/services/api";
 import SessionView from "./SessionView";
@@ -9,9 +12,7 @@ import { ISession } from "./types/Session.component";
 const Session: React.FC<ISession> = ({ children }) => {
 	const dispatch = useDispatch();
 	const token: string | null = localStorage.getItem("@taskyup.token" || null);
-	const userData: string | null = localStorage.getItem(
-		"@taskyup.user_data" || null
-	);
+	const userData: string | null = localStorage.getItem("@taskyup.user_data" || null);	
 
 	useLayoutEffect(() => {
 		if (token && userData) {
@@ -22,6 +23,21 @@ const Session: React.FC<ISession> = ({ children }) => {
 					user_data: JSON.parse(userData),
 				})
 			);
+		}
+	}, []);
+
+	const location = useLocation();	
+	const [accessLastPage, setAccessLastPage] = useLocalStorage("@taskyup.access_last_page", location.pathname);
+	const { isAuthenticated } = useSelector((state: { auth: IAuthState }) => state.auth);	
+	const navigate = useNavigate();	
+	
+	useEffect(() => {
+		setAccessLastPage(location.pathname);
+	}, [location]);
+	
+	useEffect(() => {
+		if(isAuthenticated) {
+			navigate(accessLastPage);
 		}
 	}, []);
 
