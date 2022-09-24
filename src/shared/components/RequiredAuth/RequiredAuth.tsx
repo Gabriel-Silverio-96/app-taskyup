9a1518f97a79e17d2623addf84c53a56ff75f0da
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-09-24 12:57:02 -0300

    refactor(RequiredAuth): add box component

--- src/shared/components/RequiredAuth/RequiredAuth.tsx
+++ src/shared/components/RequiredAuth/RequiredAuth.tsx
@@ -3,17 +3,24 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { IAuthState } from "shared/common/store/Auth/types/Auth.types";
 import Drawer from "../Drawer";
+import { Box } from "./style";
 import { IRequiredAuth } from "./types/RequiredAuth.component";
 
 const RequiredAuth: React.FC<IRequiredAuth> = ({ children }) => {
-	const { isAuthenticated } = useSelector((state: { auth: IAuthState }) => state.auth);
+	const { isAuthenticated } = useSelector(
+		(state: { auth: IAuthState }) => state.auth
+	);
 	return (
 		<>
 			{isAuthenticated ? (
 				<Drawer>
-					{children}
+					<Box component="section">
+						{children}	
+					</Box>
 				</Drawer>
-			) : <Navigate to="/auth/signin" />}
+			) : (
+				<Navigate to="/auth/signin" />
+			)}
 		</>
 	);
 };
---
+++ src/shared/components/RequiredAuth/style/index.ts
@@ -0,0 +1,10 @@
+import { styled, Box as MuiBox } from "@mui/material";
+
+export const Box = styled(MuiBox)(
+	({theme}) => `
+        flex-grow: 1;
+        overflow: auto;
+        height: calc(100vh - 67px);
+        padding: ${theme.spacing(3)} ${theme.spacing(3)} ${theme.spacing(16)};
+    `    
+);
