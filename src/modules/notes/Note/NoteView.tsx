25f7827bb26b8facf0472735f7cd6c5bd0622e53
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-09-24 12:56:24 -0300

    refactor(Note): remove box component

--- src/modules/notes/Note/NoteView.tsx
+++ src/modules/notes/Note/NoteView.tsx
@@ -1,21 +1,20 @@
-import { Box } from "@mui/material";
 import CardNote from "./CardNote";
-import DialogDeleteSingleNote from "./DialogDeleteSingleNote";
 import DialogDeleteAllNotes from "./DialogDeleteAllNotes";
+import DialogDeleteSingleNote from "./DialogDeleteSingleNote";
 import DialogEditNote from "./DialogEditNote";
 import DialogNewNote from "./DialogNewNote";
 import HeaderNote from "./HeaderNote";
 
 const NoteView = () => {
 	return (
-		<Box component="section" sx={{ flexGrow: 1, p: 3 }}>
+		<>
 			<HeaderNote />
 			<CardNote />
 			<DialogNewNote />
 			<DialogEditNote />
 			<DialogDeleteSingleNote />
 			<DialogDeleteAllNotes />
-		</Box>	
+		</>	
 	);
 };
 
