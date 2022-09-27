5423000e54f111a53675d8fff6361cb6304dc085
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-09-27 18:13:29 -0300

    fix(service): name env

--- src/shared/services/api.ts
+++ src/shared/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.ENVIRONMENT === "production" 
+const baseURL = process.env.REACT_APP_ENVIRONMENT === "production" 
 	? "https://api-mynotes-production.up.railway.app" : "http://localhost:4041";
 
 const ONE_MINUTE = 60000;
