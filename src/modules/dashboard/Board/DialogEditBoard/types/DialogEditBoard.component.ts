ec42cf8f8be65a81fc893994e5e9327309c12fee
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-09-28 20:46:08 -0300

    refactor(DialogEditBoard): types

--- src/modules/dashboard/Board/DialogEditBoard/types/DialogEditBoard.component.ts
+++ src/modules/dashboard/Board/DialogEditBoard/types/DialogEditBoard.component.ts
@@ -1,3 +1,18 @@
+import { FieldErrors, UseFormHandleSubmit, UseFormRegister } from "react-hook-form";
+
 export interface IDialogEditBoardForm {
-    title: string;
+    title?: string;
+    created_at?: string;
+}
+
+export interface IDialogEditBoardView {
+    register: UseFormRegister<IDialogEditBoardForm>;
+    fullScreen: boolean;
+    handleSubmit: UseFormHandleSubmit<IDialogEditBoardForm>;
+    fetchDialogEditBoard: (form: IDialogEditBoardForm) => void;
+    errors: FieldErrors<IDialogEditBoardForm>;
+    isLoading: boolean;
+    isSaving: boolean;
+    isOpenDialogEditBoard: boolean;
+    closeDialogEditBoard: () => void;
 }
