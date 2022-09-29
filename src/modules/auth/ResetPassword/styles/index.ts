4a429f76fa156b70b01841ab8c34e7a23be1266d
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-09-29 18:05:14 -0300

    style(ResetPassword): remove style title

--- src/modules/auth/ResetPassword/styles/index.ts
+++ src/modules/auth/ResetPassword/styles/index.ts
@@ -2,16 +2,6 @@ import { IconButton as IconButtonMui, styled } from "@mui/material";
 import { Link as LinkRouterDom } from "react-router-dom";
 import { ILinkStyle } from "./types";
     
-export const Title = styled("div")(
-	({ theme }) => `
-        margin: ${theme.spacing(8, 0, 5)};
-        p {
-            margin: 0;
-            color: ${theme.palette.grey[700]}
-        }
-    `   
-);
-
 export const TextFieldPassword = styled("div")({
 	position: "relative"
 });
