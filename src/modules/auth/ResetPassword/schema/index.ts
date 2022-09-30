96a53874fa5045882534908646316f323176931b
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-09-30 18:46:23 -0300

    feat(ResetPassword): schema password accept character special

--- src/modules/auth/ResetPassword/schema/index.ts
+++ src/modules/auth/ResetPassword/schema/index.ts
@@ -3,7 +3,7 @@ import * as Yup from "yup";
 const schema = Yup.object({
 	password: Yup.string()
 		.required()
-		.matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, "Min 8 characters, \n at least one letter and one number")
+		.matches(/^(?=.*\d.*\d)(?=.*[a-zA-Z].*[a-zA-Z])(?=.*[\W]).{8,}$/, "Min 8 characters \n at least two letter, two number and one character special")
 		.min(8, "Password is too short. Should be 8 chars minimum")
 });
 
