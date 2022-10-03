f009d5507953361b6dc81ae47f04791ab117bb02
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-10-02 23:08:46 -0300

    [WIP]refactor(component): create TextFieldPassword

---
+++ src/shared/components/TextFieldPassword/TextFieldPassword.tsx
@@ -0,0 +1,8 @@
+import React, { memo } from "react";
+import TextFieldPasswordView from "./TextFieldPasswordView";
+
+const TextFieldPassword: React.FC = () => {
+	return <TextFieldPasswordView />;
+};
+
+export default memo(TextFieldPassword);
---
+++ src/shared/components/TextFieldPassword/TextFieldPasswordView.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TextFieldPasswordView: React.FC<any> = () => {
+	return (
+		<h1>TextFieldPasswordView</h1>
+	);
+};
+
+export default TextFieldPasswordView;
