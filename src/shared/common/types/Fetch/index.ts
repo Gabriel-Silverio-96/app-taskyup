03b0e4bc9c22f5005652935bd6f0681659190970
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-10-08 19:15:24 -0300

    refactor(common): types status code

--- src/shared/common/types/Fetch/index.ts
+++ src/shared/common/types/Fetch/index.ts
@@ -8,3 +8,16 @@ export interface IFetchSingleBoard {
     board_type_id: string;
     created_at: string;
 }
+
+export enum EStatusSuccessCode {
+    OK = 200,
+    Created = 201,
+}
+
+export enum EStatusErrorCode {
+    Unauthorized = 401,
+    Forbidden = 403,
+    NotFound = 404,
+    TooManyRequests = 429,
+    InternalServerError = 500,			
+}
