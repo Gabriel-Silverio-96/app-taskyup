e34632f477322ba6f08b855bd8b842619502888e
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-10-09 17:07:15 -0300

    fix(Header): clean cache request when logout

--- src/shared/components/Drawer/Header/Dropdown/Dropdown.tsx
+++ src/shared/components/Drawer/Header/Dropdown/Dropdown.tsx
@@ -1,3 +1,4 @@
+import { useQueryClient } from "@tanstack/react-query";
 import React, { memo } from "react";
 import { useNavigate } from "react-router-dom";
 import DropdownView from "./DropdownView";
@@ -5,9 +6,14 @@ import { IDropdown } from "./types/Dropdown.component";
 
 const Dropdown: React.FC<IDropdown> = ({ children, open }) => {
 	const navigate = useNavigate();
+	const queryClient = useQueryClient();
+	
 	const logout = () => {
 		localStorage.removeItem("@taskyup.token");
 		localStorage.removeItem("@taskyup.user_data");
+		localStorage.removeItem("@taskyup.access_last_page");
+
+		queryClient.clear();		
 		navigate("/auth/signin");
 	};
 
