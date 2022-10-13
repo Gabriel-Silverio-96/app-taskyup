8384d46c7ab7eb3ba800db13863f730407c2adf9
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-10-12 21:15:18 -0300

    test(SignIn): user not exist

--- src/modules/auth/SignIn/__test__/SignIn.test.tsx
+++ src/modules/auth/SignIn/__test__/SignIn.test.tsx
@@ -1,16 +1,26 @@
-import { screen } from "@testing-library/react";
+import { act, cleanup, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import MockAdapter from "axios-mock-adapter";
+import api from "shared/services/api";
 import { EMAIL_MESSAGE_VALIDATION_MOCK, PASSWORD_MESSAGE_VALIDATION_MOCK } from "shared/util/test/message";
 import render from "shared/util/test/render";
 import SignIn from "../SignIn";
+import { USER_NOT_EXIST_MOCK } from "./mock";
+
+const mock = new MockAdapter(api);
+beforeAll(() => mock.reset());
+afterEach(cleanup);
 
 const { requiredEmail, validationEmail } = EMAIL_MESSAGE_VALIDATION_MOCK;
 const { validationPassword } = PASSWORD_MESSAGE_VALIDATION_MOCK;
 
-const EACH_EMAIL_CASES = [
-	{ name: /email/i, value: "Email invalid", message: validationEmail },
-	{ name: /email/i, value: "E", message: validationEmail },
-	{ name: /email/i, value: "email@email.", message: validationEmail },
+const LABEL_EMAIL = /email/i;
+const LABEL_PASSWORD = /password/i;
+
+const EACH_EMAIL_VALIDATION_CASES = [
+	{ label: LABEL_EMAIL, value: "Email invalid", message: validationEmail },
+	{ label: LABEL_EMAIL, value: "E", message: validationEmail },
+	{ label: LABEL_EMAIL, value: "email@email.", message: validationEmail },
 ];
 
 describe("Component <SignIn />", () => {
@@ -36,11 +46,11 @@ describe("Component <SignIn />", () => {
 		expect(passwordRequiredMessage).toBeInTheDocument();
 	});
 
-	test.each(EACH_EMAIL_CASES)(
+	test.each(EACH_EMAIL_VALIDATION_CASES)(
 		"Should show validation message when write wrong email",
-		async ({name, value, message}) => {
+		async ({ label, value, message }) => {
 			render(<SignIn />);
-			const inputEmail = screen.getByRole("textbox", { name });
+			const inputEmail = screen.getByLabelText(label);
 			userEvent.type(inputEmail, value);
 
 			const emailMessage = await screen.findByText(message);
@@ -50,10 +60,30 @@ describe("Component <SignIn />", () => {
 
 	test("Should show validation message when password is less than five characters", async () => {
 		render(<SignIn />);
-		const inputPassword = screen.getByLabelText(/password/i);			
+		const inputPassword = screen.getByLabelText(LABEL_PASSWORD);			
 		userEvent.type(inputPassword, "1234");
 
 		const passwordMessage = await screen.findByText(validationPassword);
 		expect(passwordMessage).toBeInTheDocument();
 	});
+
+	test(`Should show message '${USER_NOT_EXIST_MOCK.message}' when the user does not exist`, async () => {
+		mock.onPost("auth/login").reply(403, USER_NOT_EXIST_MOCK);
+		
+		render(<SignIn />);		
+		await act(() => {
+			const inputEmail = screen.getByLabelText(LABEL_EMAIL);
+			userEvent.type(inputEmail, "email@email.com");
+			const inputPassword = screen.getByLabelText(LABEL_PASSWORD);
+			userEvent.type(inputPassword, "123456");			
+				
+			const buttonSubmit = screen.getByRole("button", { name: "Sign in" });
+			userEvent.click(buttonSubmit);
+		});
+
+		await waitFor(() => {
+		 	const snackbarMessage = screen.getByText(USER_NOT_EXIST_MOCK.message);		
+		 	expect(snackbarMessage).toBeInTheDocument();
+		});
+	});	
 });
---
+++ src/modules/auth/SignIn/__test__/mock/index.ts
@@ -0,0 +1,6 @@
+const USER_NOT_EXIST_MOCK = {
+	type_message: "warning",
+	message: "Unregistered user, create an account"
+};
+
+export { USER_NOT_EXIST_MOCK };
