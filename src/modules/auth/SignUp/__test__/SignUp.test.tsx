f7245dc472be0aae2aaf1c52846acfb201abad8f
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-10-18 19:46:46 -0300

    test(SignUp): submit empty form

--- src/modules/auth/SignUp/__test__/SignUp.test.tsx
+++ src/modules/auth/SignUp/__test__/SignUp.test.tsx
@@ -1,3 +1,5 @@
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import render from "shared/util/test/render";
 import SignUp from "../SignUp";
 
@@ -12,5 +14,25 @@ describe("Component <SignUp />", () => {
 		unmount();		
 		expect(container).toBeEmptyDOMElement();
 	});    
-    
+
+	test("Should show required message full name, email and password after submitting empty form", async () => {
+		render(<SignUp />);
+		const buttonSubmit = screen.getByRole("button", { name: "Sign up" });
+		userEvent.click(buttonSubmit);
+
+		const fullNameRequiredMessage = await screen.findByText(
+			/full name is a required field/i
+		);
+
+		const emailRequiredMessage = await screen.findByText(
+			/email is a required field/i
+		);
+		const passwordRequiredMessage = await screen.findByText(
+			/password is a required field/i
+		);
+
+		expect(fullNameRequiredMessage).toBeInTheDocument();
+		expect(emailRequiredMessage).toBeInTheDocument();
+		expect(passwordRequiredMessage).toBeInTheDocument();
+	});
 });
