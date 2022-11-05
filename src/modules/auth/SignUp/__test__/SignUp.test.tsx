d15819e6d6dc726d8557e8c61353f5bd7adabc6c
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-11-05 13:45:36 -0300

    test(SignUp): request cases

--- src/modules/auth/SignUp/__test__/SignUp.test.tsx
+++ src/modules/auth/SignUp/__test__/SignUp.test.tsx
@@ -1,7 +1,33 @@
-import { screen } from "@testing-library/react";
+import { act, cleanup, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import MockAdapter from "axios-mock-adapter";
+import api from "shared/services/api";
 import render from "shared/util/test/render";
 import SignUp from "../SignUp";
+import { CREATE_ACCOUNT_SUCCESS_MOCK, EMAIL_REGISTERED_MOCK, SIGNUP_MOCK } from "./mock";
+
+const mock = new MockAdapter(api);
+beforeAll(() => mock.reset());
+afterEach(cleanup);
+
+const { fullName, email, password  } = SIGNUP_MOCK;
+
+const LABEL_FULL_NAME = /full name/i;
+const LABEL_EMAIL = /email/i;
+const LABEL_PASSWORD = /password/i;
+
+const EACH_AUTHENTICATION_REQUEST_CASES = [
+	{
+		status: 403,		
+		mockData: EMAIL_REGISTERED_MOCK,
+		messageTest: EMAIL_REGISTERED_MOCK.message,
+	},
+	{
+		status: 201,		
+		mockData: CREATE_ACCOUNT_SUCCESS_MOCK,
+		messageTest: CREATE_ACCOUNT_SUCCESS_MOCK.message,
+	},
+];
 
 describe("Component <SignUp />", () => {
 	test("Should mount the component", () => {
@@ -35,4 +61,33 @@ describe("Component <SignUp />", () => {
 		expect(emailRequiredMessage).toBeInTheDocument();
 		expect(passwordRequiredMessage).toBeInTheDocument();
 	});
+
+	test.each(EACH_AUTHENTICATION_REQUEST_CASES)(
+		"Should show message snackbar $messageTest",
+		async ({ status, mockData }) => {
+			mock.onPost("auth/create-account").reply(status, mockData);
+			
+			render(<SignUp />);
+			await act(() => {
+				const inputEmail = screen.getByLabelText(LABEL_EMAIL);
+				userEvent.type(inputEmail, email);
+
+				const inputFullname = screen.getByLabelText(LABEL_FULL_NAME);
+				userEvent.type(inputFullname, fullName);
+
+				const inputPassword = screen.getByLabelText(LABEL_PASSWORD) as any;
+				userEvent.type(inputPassword, password);
+
+				const buttonSubmit = screen.getByRole("button", {
+					name: "Sign up",
+				});
+				userEvent.click(buttonSubmit);				
+			});
+			
+			await waitFor(() => {
+			 	const snackbarMessage = screen.getByText(mockData.message);				
+				expect(snackbarMessage).toBeInTheDocument();
+			});
+		}
+	);	
 });
