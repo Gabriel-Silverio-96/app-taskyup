cc11fad778aea65433e9e9ea8ef63de39fc8603c
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-11-06 18:34:51 -0300

    refactor(ForgotPassword): state name

--- src/modules/auth/ForgotPassword/ForgotPassword.tsx
+++ src/modules/auth/ForgotPassword/ForgotPassword.tsx
@@ -14,16 +14,16 @@ const ForgotPassword: React.FC = () => {
 	} = useForm<IForgotPasswordForm>({ resolver: resolverSchema, mode: "all" });
 
 	const [isLoading, setIsLoading] = useState(false);	
-	const [emailSend, setEmailSend] = useState<IEmailSend>({ email: "", isSendEmail: false });
+	const [sendEmail, setSendEmail] = useState<IEmailSend>({ email: "", isSending: false });
 		
 	const fetchForgotPassword = async (form: IForgotPasswordForm) => {
 		try {
 			setIsLoading(true);
 			await api.post<IFetchResponseDefault>("auth/forgot-password", form);			
-			setEmailSend({ email: form.email, isSendEmail: true });
+			setSendEmail({ email: form.email, isSending: true });
 		} catch (error) {
 			console.error("ForgotPassword ", error);
-			setEmailSend( { email: "", isSendEmail: false} );
+			setSendEmail( { email: "", isSending: false} );
 		} finally {
 			setIsLoading(false);
 		}		
@@ -31,7 +31,7 @@ const ForgotPassword: React.FC = () => {
 	const onSubmit = handleSubmit(fetchForgotPassword);
 	
 	return (
-		<ForgotPasswordView {...{ register, errors, onSubmit, isLoading, emailSend }} />
+		<ForgotPasswordView {...{ register, errors, onSubmit, isLoading, sendEmail }} />
 	);
 };
 
