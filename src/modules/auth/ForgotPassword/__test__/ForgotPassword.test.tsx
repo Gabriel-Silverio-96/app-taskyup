7f44c3b27bf8f3e41122e1c9475720d73ca561a8
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-11-07 20:03:11 -0300

    test(ForgotPassword): request cases

--- src/modules/auth/ForgotPassword/__test__/ForgotPassword.test.tsx
+++ src/modules/auth/ForgotPassword/__test__/ForgotPassword.test.tsx
@@ -1,5 +1,19 @@
+import { cleanup, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import MockAdapter from "axios-mock-adapter";
+import { act } from "react-dom/test-utils";
+import api from "shared/services/api";
 import render from "shared/util/test/render";
 import ForgotPassword from "../ForgotPassword";
+import { FORGOT_PASSWORD_MOCK, UNREGISTERED_USER_MOCK, REGISTERED_USER_MOCK } from "./mock";
+
+const mock = new MockAdapter(api);
+beforeAll(() => mock.reset());
+afterEach(cleanup);
+
+const { email } = FORGOT_PASSWORD_MOCK;
+
+const LABEL_EMAIL = /email/i;
 
 describe("Component <ForgotPassword />", () => {
 	test("Should mount the component", () => {
@@ -12,4 +26,43 @@ describe("Component <ForgotPassword />", () => {
 		unmount();		
 		expect(container).toBeEmptyDOMElement();
 	}); 
+
+	test(`Should show message ${UNREGISTERED_USER_MOCK.message} when the user has no account`, async () => {
+		mock.onPost("auth/forgot-password").reply(403, UNREGISTERED_USER_MOCK);
+
+		render(<ForgotPassword />);
+		await act(() => {
+			const inputEmail = screen.getByLabelText(LABEL_EMAIL);
+			userEvent.type(inputEmail, email);
+
+			const buttonSubmit = screen.getByRole("button", { name: "Send" });		
+			userEvent.click(buttonSubmit);
+		});
+
+		await waitFor(() => {
+			const snackbarMessage = screen.getByText(UNREGISTERED_USER_MOCK.message);				
+		   	expect(snackbarMessage).toBeInTheDocument();
+	   });
+	});
+
+	test(`Should show message ${REGISTERED_USER_MOCK.message} when the user has account`, async () => {
+		mock.onPost("auth/forgot-password").reply(200, REGISTERED_USER_MOCK);
+
+		const { container } = render(<ForgotPassword />);
+		await act(() => {
+			const inputEmail = screen.getByLabelText(LABEL_EMAIL);
+			userEvent.type(inputEmail, email);				
+			
+			const buttonSubmit = screen.getByRole("button", { name: "Send" });		
+			userEvent.click(buttonSubmit);
+		});
+
+		await waitFor(() => {
+			const snackbarMessage = screen.getByText(REGISTERED_USER_MOCK.message);				
+			expect(snackbarMessage).toBeInTheDocument();
+
+			const form = container.querySelector("form");
+			expect(form).not.toBeInTheDocument();
+	   });
+	});
 });
