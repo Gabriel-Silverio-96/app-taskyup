2a4e6dfafd73eace477d8a83732ad955c7050ac7
Gabriel Silvério <53228013+Gabriel-Silverio-96@users.noreply.github.com>
2022-11-10 21:37:58 -0300

    unit-test(ResetPassword): component (#28)
    * test(ResetPassword): mount and unmount component
    
    * chore(util): render component with route path
    
    * (WIP) test(ResetPassword): request invalid token
    
    * chore(util): render component with route path interceptor included
    
    * test(ResetPassword): url token
    
    * test(ResetPassword): show success message when password was reseted

---
+++ src/modules/auth/ResetPassword/__test__/ResetPassword.test.tsx
@@ -0,0 +1,92 @@
+import { act, cleanup, prettyDOM, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import MockAdapter from "axios-mock-adapter";
+import api from "shared/services/api";
+import render from "shared/util/test/render";
+import {
+	renderRoutePath,
+	routePathTest
+} from "shared/util/test/renderRoutePath";
+import ResetPassword from "../ResetPassword";
+import { INVALID_TOKEN_MOCK, RESET_PASSWORD_MOCK, RESET_PASSWORD_SUCCESS_MOCK } from "./mock";
+
+const mock = new MockAdapter(api);
+beforeAll(() => mock.reset());
+afterEach(cleanup);
+
+const { token, password } = RESET_PASSWORD_MOCK;
+
+const LABEL_PASSWORD = /password/i;
+
+describe("Component <ResetPassword />", () => {
+	test("Should mount the component", () => {
+		const { container } = render(<ResetPassword />);
+		expect(container).toBeDefined();
+	});
+
+	test("Should unmount the component", () => {
+		const { container, unmount } = render(<ResetPassword />);
+		unmount();
+		expect(container).toBeEmptyDOMElement();
+	});
+
+	test("Should match url token with mock token", async () => {
+		const history = routePathTest({ route: `/auth/reset-password/${token}` });
+		renderRoutePath(<ResetPassword />, {
+			path: "/auth/reset-password/:token",
+			location: history.location,
+			history,
+		});
+		const tokenURL = history.location.pathname.split("/").at(-1);			
+		
+		expect(tokenURL).toEqual(token);
+	});
+
+	test("Should show message when token is invalid ", async () => {
+		mock.onPost(`auth/reset-password/token=${token}`).reply(500, INVALID_TOKEN_MOCK);
+		
+		const history = routePathTest({ route: `/auth/reset-password/${token}` });
+		renderRoutePath(<ResetPassword />, {
+			path: "/auth/reset-password/:token",
+			location: history.location,
+			history,
+		});
+
+		await act(() => {
+			const inputPassword = screen.getByLabelText(LABEL_PASSWORD);
+			userEvent.type(inputPassword, password);				
+			
+			const buttonSubmit = screen.getByRole("button", { name: "Save" });		
+			userEvent.click(buttonSubmit);
+		});
+
+		await waitFor(() => {
+			const snackbarMessage = screen.getByText(INVALID_TOKEN_MOCK.message);				
+			expect(snackbarMessage).toBeInTheDocument();
+		});
+	});
+
+	test("Should show success message when password was reseted", async () => {
+		mock.onPost(`auth/reset-password/token=${token}`).reply(200, RESET_PASSWORD_SUCCESS_MOCK);
+		
+		const history = routePathTest({ route: `/auth/reset-password/${token}` });
+		renderRoutePath(<ResetPassword />, {
+			path: "/auth/reset-password/:token",
+			location: history.location,
+			history,
+		});
+
+		await act(() => {
+			const inputPassword = screen.getByLabelText(LABEL_PASSWORD);
+			userEvent.type(inputPassword, password);				
+			
+			const buttonSubmit = screen.getByRole("button", { name: "Save" });		
+			userEvent.click(buttonSubmit);
+		});
+
+		await waitFor(() => {
+			const snackbarMessage = screen.getByText(RESET_PASSWORD_SUCCESS_MOCK.message);				
+			expect(snackbarMessage).toBeInTheDocument();
+		});
+	});
+});
---
+++ src/modules/auth/ResetPassword/__test__/mock/index.ts
@@ -0,0 +1,19 @@
+import { faker } from "@faker-js/faker";
+
+const RESET_PASSWORD_MOCK = {
+	token: faker.lorem.word(),
+	password: faker.internet.password(20, true, /[a-z]/, "11546@")
+};
+
+const INVALID_TOKEN_MOCK = {
+	err: { name: faker.lorem.sentence(), message: faker.lorem.sentence() },
+	message: "Invalid token",
+	type_message: "error",
+};
+
+const RESET_PASSWORD_SUCCESS_MOCK = {
+	message: "Password updated",
+	type_message: "success",
+};
+
+export { RESET_PASSWORD_MOCK, INVALID_TOKEN_MOCK, RESET_PASSWORD_SUCCESS_MOCK };
