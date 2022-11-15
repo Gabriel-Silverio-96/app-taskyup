26a6c47a36259c5b44bf4fcb7c1baac38a24eb4f
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-11-15 13:23:17 -0300

    test(Board): create new board type notes

--- src/modules/dashboard/Board/__test__/Board.test.tsx
+++ src/modules/dashboard/Board/__test__/Board.test.tsx
@@ -1,12 +1,19 @@
-import { cleanup } from "@testing-library/react";
+import { cleanup, fireEvent, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import MockAdapter from "axios-mock-adapter";
+import { act } from "react-dom/test-utils";
 import api from "shared/services/api";
 import renderRequiredAuth from "shared/util/test/renderRequiredAuth";
 import Board from "../Board";
+import { BOARD_MOCK, CREATE_BOARD_SUCCESS_REPOSNSE_MOCK } from "./mock";
 
 const mock = new MockAdapter(api);
 beforeAll(() => mock.reset());
 afterEach(cleanup);
+
+const { board_name, board_type_id } = BOARD_MOCK;
+
+const LABEL_BOARD_NAME = /board name/i;
 
 describe("Component <Board />", () => {
 	test("Should mount the component", () => {
@@ -19,4 +26,29 @@ describe("Component <Board />", () => {
 		unmount();
 		expect(container).toBeEmptyDOMElement();
 	});	
+
+	test("Create new board type notes", async () => {
+		mock.onPost("board/create").reply(201, CREATE_BOARD_SUCCESS_REPOSNSE_MOCK);
+		renderRequiredAuth(<Board />);
+
+		const buttonNewBoard = screen.getByRole("button", { name: "New board" });
+		userEvent.click(buttonNewBoard);
+		
+		await act(() => {		
+			const inputBoardName = screen.getByLabelText(LABEL_BOARD_NAME);
+			userEvent.type(inputBoardName, board_name);
+
+			const selectBoardType = screen.getByTestId("board-type-id-input-inside-select");
+			fireEvent.change(selectBoardType, { target: { value: board_type_id.notes }});			
+			
+			const buttonSubmit = screen.getByRole("button", { name: "Save" });
+			userEvent.click(buttonSubmit);			
+		});
+
+		await waitFor(() => {			
+			const snackbarMessage = screen.getByText(CREATE_BOARD_SUCCESS_REPOSNSE_MOCK.message);
+			expect(snackbarMessage).toBeInTheDocument();
+		});				
+	});
 });
+ 
