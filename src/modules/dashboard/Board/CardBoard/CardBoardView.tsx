94111bcb75177a04cff8dec713d27549eb8b037e
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-12-23 19:14:02 -0300

    refactor(Board): remove menu item background

--- src/modules/dashboard/Board/CardBoard/CardBoardView.tsx
+++ src/modules/dashboard/Board/CardBoard/CardBoardView.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent, Grid, IconButton, MenuItem, Typography } from "@mui/
 import React, { MouseEvent } from "react";
 import { AiOutlineDelete } from "react-icons/ai";
 import { BsThreeDots } from "react-icons/bs";
-import { FiEdit, FiImage } from "react-icons/fi";
+import { FiEdit } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import { IFetchBoard } from "shared/common/hook/useFetchBoard/types/UseFetchBoard.types";
 import Loading from "shared/components/Loading";
@@ -72,9 +72,6 @@ const CardBoardView: React.FC<ICardBoardView> = props => {
 											>
 												<MenuItem onClick={() => openDialogEditBoard(closeMenu)}>
 													<FiEdit /> Edit
-												</MenuItem>
-												<MenuItem disabled>
-													<FiImage /> Background
 												</MenuItem>
 												<MenuItem onClick={() => openDialogDeleteSingleBoard(closeMenu)} 
 													sx={ { color: palette.error.main } }
