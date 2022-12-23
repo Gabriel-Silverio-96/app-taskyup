ae1a3a83967f9e559da4bb11c05b1b497b3ec709
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-12-23 19:44:14 -0300

    feat(Board): types fetch board

--- src/shared/common/types/Fetch/index.ts
+++ src/shared/common/types/Fetch/index.ts
@@ -6,6 +6,7 @@ export interface IFetchResponseDefault {
 export interface IFetchSingleBoard {
     title: string;
     board_type_id: string;
+    background_image: string;
     created_at: string;
 }
 
