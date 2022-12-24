809523116c1d14f767067ed7e9c5e50d3bb8fda8
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2022-12-24 19:30:02 -0300

    refactor(Board): create folder constant

--- src/modules/dashboard/Board/DialogEditBoard/DialogBackground/DialogBackground.tsx
+++ src/modules/dashboard/Board/DialogEditBoard/DialogBackground/DialogBackground.tsx
@@ -1,30 +1,6 @@
-import React, { useState } from "react";
+import React, { memo, useState } from "react";
+import { INITIAL_STATE_IMAGES } from "./constant";
 import DialogBackgroundView from "./DialogBackgroundView";
-
-const INITIAL_STATE_IMAGES = [
-	{
-		id: 1,
-		image_url:
-			"https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-		photographer_name: "Lum3n",
-		photographer_url: "https://www.pexels.com/pt-br/@lum3n-44775/",
-	},
-
-	{
-		id: 2,
-		image_url:
-			"https://images.pexels.com/photos/14840714/pexels-photo-14840714.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-		photographer_name: "Lum3n",
-		photographer_url: "https://www.pexels.com/pt-br/@lum3n-44775/",
-	},
-	{
-		id: 3,
-		image_url:
-			"https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-		photographer_name: "Lum3n",
-		photographer_url: "https://www.pexels.com/pt-br/@lum3n-44775/",
-	},
-];
 
 const DialogBackground: React.FC = () => {
 	const [images, setImages] = useState(INITIAL_STATE_IMAGES);
@@ -50,4 +26,4 @@ const DialogBackground: React.FC = () => {
 	);
 };
 
-export default DialogBackground;
+export default memo(DialogBackground);
---
+++ src/modules/dashboard/Board/DialogEditBoard/DialogBackground/constant/index.ts
@@ -0,0 +1,23 @@
+export const INITIAL_STATE_IMAGES = [
+	{
+		id: 1,
+		image_url:
+			"https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+		photographer_name: "Lum3n",
+		photographer_url: "https://www.pexels.com/pt-br/@lum3n-44775/",
+	},
+	{
+		id: 2,
+		image_url:
+			"https://images.pexels.com/photos/14840714/pexels-photo-14840714.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+		photographer_name: "Lum3n",
+		photographer_url: "https://www.pexels.com/pt-br/@lum3n-44775/",
+	},
+	{
+		id: 3,
+		image_url:
+			"https://images.pexels.com/photos/406014/pexels-photo-406014.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+		photographer_name: "Lum3n",
+		photographer_url: "https://www.pexels.com/pt-br/@lum3n-44775/",
+	},
+];
