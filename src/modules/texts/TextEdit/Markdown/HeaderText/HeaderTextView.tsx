8750cb254dd8be4a86836295534b77b16254c641
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-01-05 19:14:53 -0300

    feat(texts): handle error message

--- src/modules/texts/TextEdit/Markdown/HeaderText/HeaderTextView.tsx
+++ src/modules/texts/TextEdit/Markdown/HeaderText/HeaderTextView.tsx
@@ -1,17 +1,35 @@
-import { Button, Grid } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import React from "react";
-import { Nav, TextField } from "./style";
+import { Nav, TextTitle, TextField } from "./style";
 import { IHeaderText } from "./types/HeaderText.component";
 
-const HeaderTextView: React.FC<IHeaderText> = ({ saveText, titleText, onChangeTextTitle }) => {
+const HeaderTextView: React.FC<IHeaderText> = ({
+	saveText,
+	titleText,
+	onChangeTextTitle,
+}) => {
+	const disableButtonSave = titleText.length < 3;
+	const handleMessageError = titleText.length < 3 && "Title text is required and has a minimum of 3 characters";
+
 	return (
 		<Grid container alignItems="center" sx={{ mb: 3 }}>
 			<Grid item xl={6} md={6} sm={8} xs={8}>
-				<TextField type="text" value={titleText} onChange={onChangeTextTitle} />
+				<TextTitle>
+					<TextField					
+						value={titleText}
+						onChange={onChangeTextTitle}
+					/>
+					<Typography variant="caption">{handleMessageError}</Typography>
+				</TextTitle>
 			</Grid>
 			<Grid item xl={6} md={6} sm={4} xs={4}>
 				<Nav>
-					<Button variant="contained" onClick={saveText}>Save</Button>  				
+					<Button
+						variant="contained"
+						onClick={saveText}
+						disabled={disableButtonSave}>
+						Save
+					</Button>
 				</Nav>
 			</Grid>
 		</Grid>
--- src/modules/texts/TextEdit/Markdown/HeaderText/style/index.ts
+++ src/modules/texts/TextEdit/Markdown/HeaderText/style/index.ts
@@ -8,6 +8,17 @@ export const Nav = styled("nav")(
     `
 );
 
+export const TextTitle = styled("div")(
+	({ theme }) => `
+        display: flex;
+        flex-direction: column;
+
+        span {
+            color: ${theme.palette.error.main};
+        }
+    `
+);
+
 export const TextField = styled("input")(
 	({ theme }) => `
         font-family: ${theme.typography.fontFamily};
@@ -18,6 +29,7 @@ export const TextField = styled("input")(
         border: 1px solid transparent;        
         padding: 0.5rem 0.8rem;
         transition: .3s;
+        width: min-content;
 
         :hover {
             border-color: ${theme.palette.grey[700]};
