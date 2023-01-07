8f062cb7e8efc8cbcc710775f0e744139eda5da2
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-01-06 21:48:37 -0300

    feat(drawer): enable create text

--- src/shared/components/Drawer/Aside/DialogNewBoard/shared/schema/index.ts
+++ src/shared/components/Drawer/Aside/DialogNewBoard/shared/schema/index.ts
@@ -1,6 +1,6 @@
 import * as Yup from "yup";
 
-const boardTypeID = ["1f819d7d-11b5-4742-be86-2286ef50b5d5", "7b784aee-0133-4d6a-b14b-8dbbe19e9428", "1cf4ff82-59bd-4733-890c-c9226ea0df2f"];
+const boardTypeID = ["1f819d7d-11b5-4742-be86-2286ef50b5d5", "43fa2ae7-48eb-4e1c-a746-617a643fee45", "1cf4ff82-59bd-4733-890c-c9226ea0df2f"];
 
 const schema = Yup.object({
 	title: Yup.string().required()
