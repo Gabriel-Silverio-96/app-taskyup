12ea71ee5253705edebdb8e9bde4a9fda7eb987d
Gabriel Silvério <53228013+Gabriel-Silverio-96@users.noreply.github.com>
2023-01-09 18:51:08 -0300

    refactor(dashboard): dialog edit board (#43)
    * refactor(dashboard): fetch single board

--- src/modules/dashboard/Board/DialogEditBoard/DialogEditBoard.tsx
+++ src/modules/dashboard/Board/DialogEditBoard/DialogEditBoard.tsx
@@ -4,22 +4,27 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { memo, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { IFetchSingleBoard } from "shared/common/types/Fetch";
-import api from "shared/services/api";
 import dateFormat from "shared/util/dateFormat";
 import { useContextBoard } from "../Context";
 import useDialogBoard from "../shared/hook/useDialogBoard";
 import DialogEditBoardView from "./DialogEditBoardView";
 import schema from "./schema";
+import { fetchEditBoard, fetchSingleBoard } from "./service";
 import { IDialogEditBoardForm } from "./types/DialogEditBoard.component";
 
 const DialogEditBoard = () => {
 	const theme = useTheme();
 	const queryClient = useQueryClient();
-	const { boardID, isOpenDialogEditBoard, dialogBackgroundImage ,setDialogBackgroundImage } = useContextBoard();
-	
+	const {
+		boardID,
+		isOpenDialogEditBoard,
+		dialogBackgroundImage,
+		setDialogBackgroundImage,
+	} = useContextBoard();
+
 	const { closeDialogEditBoard } = useDialogBoard();
 	const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
-	
+
 	const {
 		register,
 		handleSubmit,
@@ -27,43 +32,46 @@ const DialogEditBoard = () => {
 		setValue,
 	} = useForm({ resolver: yupResolver(schema), mode: "all" });
 
-	const fetchSingleBoard = async () => {
-		const { data } = await api.get(`/board/board_id=${boardID}`);
-		return data;
-	};
-
 	const onSuccessQuery = (data: IFetchSingleBoard) => {
 		setValue("title", data.title);
 		setValue("created_at", dateFormat(data.created_at));
 		setDialogBackgroundImage(data.background_image);
 	};
+	const optionQuery = {
+		onSuccess: onSuccessQuery,
+		cacheTime: 0,
+		retry: false,
+		enabled: false,
+	};
 
-	const { refetch, isFetching: isLoading } = useQuery<IFetchSingleBoard>(
+	const { refetch, isFetching: isLoading } = useQuery(
 		["dialog_edit_board"],
-		fetchSingleBoard,
-		{ cacheTime: 0, onSuccess: onSuccessQuery, retry: false, enabled: false }
+		() => fetchSingleBoard(boardID),
+		optionQuery
 	);
 
 	useEffect(() => {
-		if(isOpenDialogEditBoard) {
+		if (isOpenDialogEditBoard) {
 			refetch();
 		}
-		return () => setDialogBackgroundImage("");		
+		return () => setDialogBackgroundImage("");
 	}, [isOpenDialogEditBoard]);
 
-	const onSuccessMutation = () => Promise.all([
-		queryClient.invalidateQueries(["board"]),
-		queryClient.invalidateQueries(["menu"])
-	]);
+	const onSuccessMutation = () => {
+		Promise.all([
+			queryClient.invalidateQueries(["board"]),
+			queryClient.invalidateQueries(["menu"]),
+		]);
+		closeDialogEditBoard();
+	};
+	const optionMutation = { onSuccess: onSuccessMutation };
 
-	
+	const mutationFetchEditBoard = async (dataEdited: IDialogEditBoardForm) => {
+		await fetchEditBoard(dataEdited, boardID, dialogBackgroundImage);
+	};
+
 	const { mutate: fetchDialogEditBoard, isLoading: isSaving } = useMutation(
-		async (dataEditBoard: IDialogEditBoardForm) => {
-			const data = { ...dataEditBoard, background_image: dialogBackgroundImage };
-			await api.patch(`board/edit/board_id=${boardID}`, data);
-			
-			closeDialogEditBoard();
-		}, { onSuccess: onSuccessMutation }		
+		mutationFetchEditBoard, optionMutation		
 	);
 
 	return (
