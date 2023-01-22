6ad95dafb744c4aebfb5b7f999b16617f6c6e545
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-01-22 09:25:46 -0300

    refactor(notes): remove setQueryData

--- src/modules/notes/Note/CardNote/CardNote.tsx
+++ src/modules/notes/Note/CardNote/CardNote.tsx
@@ -1,22 +1,20 @@
 import { useTheme } from "@mui/material";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import React, { memo, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import React, { memo } from "react";
 import { useParams } from "react-router-dom";
 import api from "shared/services/api";
 import { useContextNote } from "../Context";
 import useDialogNote from "../shared/hook/useDialogNote";
 import CardNoteView from "./CardNoteView";
-import { IListNotes } from "./types/CardNote.component";
 
 const CardNote: React.FC = () => {
-	const { board_id: boardID } = useParams();
+	const { board_id } = useParams();
 	const { setTotalOfNotes } = useContextNote();
-	const { palette } = useTheme();
-	const queryClient = useQueryClient();
+	const { palette } = useTheme();	
 	const { openDialogEditNote, openDialogDeleteSingleNote } = useDialogNote();
 
 	const fetchNotes = async () => {
-		const { data } = await api.get(`/notes/list/board_id=${boardID}`);
+		const { data } = await api.get(`/notes/list/board_id=${board_id}`);
 		return data;
 	};
 
@@ -24,18 +22,8 @@ const CardNote: React.FC = () => {
 
 	const {
 		data: notes,
-		refetch,
 		isFetching: isLoading,
-	} = useQuery(["notes"], fetchNotes, { onSuccess });
-
-	useEffect(() => {
-		refetch();
-		const leaveEmptyListNotesUnmount = () => {
-			const unmountDataListNotesEmpty = { notes: { list_notes: [] }};
-			queryClient.setQueryData<IListNotes | any>(["notes"], unmountDataListNotesEmpty );
-		};
-		return () => leaveEmptyListNotesUnmount();
-	}, [boardID]);
+	} = useQuery(["notes", { variable: board_id }], fetchNotes, { onSuccess });
 
 	return (
 		<CardNoteView
