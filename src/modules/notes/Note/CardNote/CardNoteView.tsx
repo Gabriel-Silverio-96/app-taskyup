5e643e7ae4782e973986fd87b07beae8128aa139
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-01-22 10:07:05 -0300

    style(notes): text ellipsis

--- src/modules/notes/Note/CardNote/CardNoteView.tsx
+++ src/modules/notes/Note/CardNote/CardNoteView.tsx
@@ -1,9 +1,9 @@
-import { CardContent, Grid, IconButton, Typography } from "@mui/material";
+import { Grid, IconButton, Typography } from "@mui/material";
 import React from "react";
 import { FiEye, FiTrash2 } from "react-icons/fi";
 import Loading from "shared/components/Loading";
 import NoteNotCreatedMessage from "./NoteNotCreatedMessage";
-import { Card, CardAction, CardBar, CardNoteContainer } from "./style";
+import { Card, CardContent, CardAction, CardBar, CardNoteContainer } from "./style";
 import { ICardNotesView } from "./types/CardNote.component";
 
 const CardNoteView: React.FC<ICardNotesView> = props => {
@@ -12,30 +12,23 @@ const CardNoteView: React.FC<ICardNotesView> = props => {
 		<Grid container spacing={2}>
 			<Loading isLoading={isFetching} backdrop />
 			<NoteNotCreatedMessage notes={data}/>
+			
 			{data &&
-				data.list_notes?.map(({ note_id, title_note, observation, color_note }) => {
-					const title = title_note.length > 30 
-						? `${title_note.substring(0, 30).trim()}...`
-						: title_note;
-
-					const observationNote = observation.length > 50 
-						? `${observation.substring(0, 50).trim()}...`
-						: observation;
-
+				data.list_notes?.map(({ note_id, title_note, observation, color_note }) => {	
 					return (
 						<Grid item xl={2} md={3} xs={12} key={note_id}>
 							<CardNoteContainer>
 								<Card sx={{ height: 130 }}>
 									<CardContent>
 										<Typography variant="body1" fontWeight={800}>
-											{title}
+											{title_note}
 										</Typography>
 										<Typography
 											variant="body2"
 											fontSize={13}
 											color="GrayText"
 											sx={{ mt: 1 }}>
-											{observationNote}
+											{observation}
 										</Typography>
 										<CardAction id="card-action">
 											<IconButton
