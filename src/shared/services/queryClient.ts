1227e171c9bba95a9e26d24faf907e659988e071
Gabriel Silvério <53228013+Gabriel-Silverio-96@users.noreply.github.com>
2023-01-24 20:54:53 -0300

    refactor(service): staleTime and cacheTime (#49)
    * refactor(services): staleTime and cacheTime
    
    * refactor(module): remove cacheTime

--- src/shared/services/queryClient.ts
+++ src/shared/services/queryClient.ts
@@ -1,14 +1,15 @@
 import { QueryClient } from "@tanstack/react-query";
 
 const ONE_HOUR_IN_MILLISECOND = 1000 * 60 * 60;
-const ONE_SECOND_IN_MILLISECOND = 1000;
+const ONE_SECOND = 1000;
 
 const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
 			staleTime: ONE_HOUR_IN_MILLISECOND,
+			cacheTime: ONE_HOUR_IN_MILLISECOND,
 			retry: 3,	
-			retryDelay: ONE_SECOND_IN_MILLISECOND 		
+			retryDelay: ONE_SECOND 		
 		},
 	},
 });
