0fcf310debc5b33ad02ac284391b34d010d419ee
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-01-28 20:13:15 -0300

    refactor(notes): rename variable

--- src/modules/notes/Note/HeaderNote/HeaderNote.tsx
+++ src/modules/notes/Note/HeaderNote/HeaderNote.tsx
@@ -29,9 +29,9 @@ const HeaderNote: React.FC = () => {
 
 	const queryKey = ["single_board", { variable: boardID }];
 	const options = { onError: () => navigate("/dashboard") };
-	const queryFetch = () => fetchSingleBoard(boardID);
+	const queryFn = () => fetchSingleBoard(boardID);
 
-	const { data, isFetching } = useQuery<IFetchSingleBoard>(queryKey, queryFetch, options);
+	const { data, isFetching } = useQuery<IFetchSingleBoard>(queryKey, queryFn, options);
 
 	return (
 		<HeaderNoteView
