1c5c41b8beb1fd086645f411dc96cebaac8f362b
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-02-02 19:31:42 -0300

    refactor(signup): service

--- src/modules/auth/SignUp/SignUp.tsx
+++ src/modules/auth/SignUp/SignUp.tsx
@@ -1,33 +1,35 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import { AxiosResponse } from "axios";
 import React, { useCallback, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-import { IFetchResponseDefault } from "shared/common/types/Fetch";
-import api from "shared/services/api";
 import schema from "./schema";
+import fetchSignUp from "./service";
 import SignUpView from "./SignUpView";
 import { ISignUpForm } from "./types/SignUp.component";
 
 const SignUp: React.FC = () => {
-	const { register, handleSubmit, formState: { errors } }
-		= useForm<ISignUpForm>({ resolver: yupResolver(schema), mode: "all" });
+	const { 
+		register, 
+		handleSubmit, 
+		formState: { errors } 
+	} = useForm<ISignUpForm>({ resolver: yupResolver(schema), mode: "all" });
 
 	const navigate = useNavigate();
 	const [isLoading, setIsLoading] = useState(false);
 
-	const fetchSignUp = useCallback(async (dataUser: ISignUpForm) => {
+	const signUpSubmit = useCallback(async (form: ISignUpForm) => {
 		try {
 			setIsLoading(true);
-			await api.post("auth/create-account", dataUser) as AxiosResponse<IFetchResponseDefault>;
-			navigate("/auth/signin");
+			await fetchSignUp(form);
+			
+			return navigate("/auth/signin");
 		} catch (error) {
 			setIsLoading(false);
 			console.error("SignUp ", error);
 		}
 	}, []);
 
-	return <SignUpView {...{ register, errors, handleSubmit, fetchSignUp, isLoading }} />;
+	return <SignUpView {...{ register, errors, handleSubmit, signUpSubmit, isLoading }} />;
 };
 
 export default SignUp;
