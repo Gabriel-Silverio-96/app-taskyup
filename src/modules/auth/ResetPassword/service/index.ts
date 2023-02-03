d84d54b774ee31574cb81717e73c6c855bc2fc7f
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-02-03 19:39:17 -0300

    refactor(reset-password): types service

--- src/modules/auth/ResetPassword/service/index.ts
+++ src/modules/auth/ResetPassword/service/index.ts
@@ -1,9 +1,10 @@
+import { IFetchResponseDefault } from "shared/common/types/Fetch";
 import api from "shared/services/api";
 import { IFetchResetPassword } from "../types";
 
 const fetchResetPassword = async (payload: IFetchResetPassword) => {
 	const { form, token } = payload;
-	const response = await api.post(`auth/reset-password/token=${token}`, form);
+	const response = await api.post<IFetchResponseDefault>(`auth/reset-password/token=${token}`, form);
 
 	return response;
 };
