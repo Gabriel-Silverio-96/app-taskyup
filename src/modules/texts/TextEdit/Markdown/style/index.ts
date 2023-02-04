4c247be9cbbf60bf5790235bac8dc1e9daca3d58
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-02-04 14:34:09 -0300

    style(text): markdown icons

--- src/modules/texts/TextEdit/Markdown/style/index.ts
+++ src/modules/texts/TextEdit/Markdown/style/index.ts
@@ -3,12 +3,62 @@ import { styled } from "@mui/material";
 export const Markdown = styled("section")(    
 	({ theme }) => `
         .bytemd {
+            font-family: 'Inter', sans-serif;
             top: ${theme.spacing(11)} !important;
             position: fixed;
             inset: 0;
             border: none;
             height: auto !important;      
-            filter: invert(1);
+            
+
+            .bytemd-toolbar {
+                background-color: ${theme.palette.common.black};
+
+                .bytemd-toolbar-left {
+                    .bytemd-toolbar-icon {
+                        svg {
+                            color: ${theme.palette.common.white};
+                        }
+                        :hover {
+                            background-color: ${theme.palette.primary.main};
+                        }
+                    }
+                }
+                
+                .bytemd-toolbar-right {
+                    .bytemd-toolbar-icon {
+                        svg {
+                            color: ${theme.palette.common.white};
+                        }
+                        :hover {
+                            background-color: ${theme.palette.primary.main};
+                        }
+                    }
+
+                    div:nth-of-type(5), div:nth-of-type(6) {
+                        display: none;
+                    }
+                }
+
+            .tippy-content {
+                padding: 0;
+            }
+
+            .bytemd-dropdown {
+                background-color: ${theme.palette.background.paper};
+                color: ${theme.palette.common.white};
+
+                .bytemd-dropdown-title {
+                    display: none;
+                }
+
+                .bytemd-dropdown-item {                    
+                    :hover {
+                        color: ${theme.palette.primary.main};
+                        background-color: transparent;
+                    }
+                }
+            }
         }       
     `
 );
