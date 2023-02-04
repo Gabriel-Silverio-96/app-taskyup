487c469b8a6e8a7d5f8874133e14466eed9513c5
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-02-03 22:45:53 -0300

    refactor(text): import markdown-it

--- src/modules/texts/TextEdit/Markdown/types/Markdown.component.ts
+++ src/modules/texts/TextEdit/Markdown/types/Markdown.component.ts
@@ -1,7 +1,3 @@
-import { HtmlType } from "react-markdown-editor-lite/cjs/editor/preview";
-
-export type TRenderHTML = HtmlType | Promise<HtmlType>;
-
 export interface IData {
     text: string;
     title_text: string;
