3026b97255dc79fbfe256023c74394b6df2c3e98
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-02-06 20:46:45 -0300

    feat(text): import @bytemd/plugin-gemoji

--- src/modules/texts/TextEdit/Markdown/constant/index.ts
+++ src/modules/texts/TextEdit/Markdown/constant/index.ts
@@ -1,7 +1,8 @@
 import mermaid from "@bytemd/plugin-mermaid";
 import frontmatter from "@bytemd/plugin-frontmatter";
 import gfm from "@bytemd/plugin-gfm";
+import gemoji from "@bytemd/plugin-gemoji";
 
 export const INITIAL_STATE_DATA = { title_board: "", title_text: "", text: "", created_at: "" };
 
-export const PLUGINS = [frontmatter(), gfm(), mermaid()];
+export const PLUGINS = [frontmatter(), gfm(), mermaid(), gemoji()];
