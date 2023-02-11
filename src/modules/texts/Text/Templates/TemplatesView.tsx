f724bd4d7165c009ee1d7243f716dd5c2e066c87
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-02-10 21:41:28 -0300

    style(text): scroll template

--- src/modules/texts/Text/Templates/TemplatesView.tsx
+++ src/modules/texts/Text/Templates/TemplatesView.tsx
@@ -5,47 +5,53 @@ import IconDiagram from "./components/IconDiagram";
 import IconReadme from "./components/IconReadme";
 import IconTable from "./components/IconTable";
 import IconTodo from "./components/IconTodo";
-import { CardContent, CardTemplate, Templates } from "./style";
+import { CardContent, CardTemplate, TemplateContainer, Templates, TemplateLinearGradient } from "./style";
 
 const TemplatesView: React.FC<any> = (props) => {	
 	const { isOpenTemplates, createTextTemplate } = props;
-	return (
-		<Templates open={isOpenTemplates}>
-			<CardTemplate onClick={createTextTemplate}>
-				<CardContent>
-					<IconTable />
-				</CardContent>
-				<Typography variant="caption">Table</Typography>
-			</CardTemplate>
+	return (		
+		<TemplateContainer open={isOpenTemplates}>
+			<Templates>		
+				<Typography variant="caption">Choose template</Typography>
+				<div>
+					<CardTemplate onClick={createTextTemplate}>
+						<CardContent>
+							<IconTable />
+						</CardContent>
+						<Typography variant="caption">Table</Typography>
+					</CardTemplate>
 				
-			<CardTemplate onClick={() => createTextTemplate("todo")}>
-				<CardContent>
-					<IconTodo />
-				</CardContent>
-				<Typography variant="caption">Todo</Typography>
-			</CardTemplate>
+					<CardTemplate onClick={() => createTextTemplate("todo")}>
+						<CardContent>
+							<IconTodo />
+						</CardContent>
+						<Typography variant="caption">Todo</Typography>
+					</CardTemplate>
 				
-			<CardTemplate onClick={() => createTextTemplate("readme")}>
-				<CardContent>
-					<IconReadme />
-				</CardContent>
-				<Typography variant="caption">Readme</Typography>
-			</CardTemplate>
+					<CardTemplate onClick={() => createTextTemplate("readme")}>
+						<CardContent>
+							<IconReadme />
+						</CardContent>
+						<Typography variant="caption">Readme</Typography>
+					</CardTemplate>
 
-			<CardTemplate onClick={() => createTextTemplate("code")}>
-				<CardContent>
-					<IconCode />
-				</CardContent>
-				<Typography variant="caption">Code</Typography>
-			</CardTemplate>	
+					<CardTemplate onClick={() => createTextTemplate("code")}>
+						<CardContent>
+							<IconCode />
+						</CardContent>
+						<Typography variant="caption">Code</Typography>
+					</CardTemplate>	
 
-			<CardTemplate onClick={() => createTextTemplate("diagram")}>
-				<CardContent>
-					<IconDiagram />
-				</CardContent>
-				<Typography variant="caption">Diagram</Typography>
-			</CardTemplate>					
-		</Templates>
+					<CardTemplate onClick={() => createTextTemplate("diagram")}>
+						<CardContent>
+							<IconDiagram />
+						</CardContent>
+						<Typography variant="caption">Diagram</Typography>
+					</CardTemplate>		
+				</div>	
+			</Templates>
+			<TemplateLinearGradient />
+		</TemplateContainer>
 	);
 };
 
--- src/modules/texts/Text/Templates/style/index.ts
+++ src/modules/texts/Text/Templates/style/index.ts
@@ -1,17 +1,43 @@
 import { styled } from "@mui/material";
 import { ITemplatesStyled } from "./types";
 
-export const Templates = styled("section")<ITemplatesStyled>(
-	({ theme, open }) => `
+export const TemplateContainer = styled("section")<ITemplatesStyled>(
+	({ open }) => `
+        display: ${open ? "block" : "none"};       
+        position: relative;
+    `
+);
+
+export const Templates = styled("div")(
+	({ theme }) => `
         width: auto;
-        height: ${theme.spacing(30)};
-        padding: ${theme.spacing(5)}; 
+        height: ${theme.spacing(31.25)};
+        padding: ${theme.spacing(3)}; 
         border-radius: ${theme.spacing(1)};
         background-color: ${theme.palette.grey[900]};
         border: 1px solid ${theme.palette.grey[800]};
-        margin-bottom: ${theme.spacing(3)};
-        display: ${open ? "flex" : "none"};
-        gap: ${theme.spacing(2)};        
+        margin-bottom: ${theme.spacing(3)};        
+        overflow-y: hidden;                       
+
+        
+        ::-webkit-scrollbar {
+            width: 0 !important;
+            height: 0 !important;
+            background:transparent !important;
+        }
+        ::-webkit-scrollbar-thumb {
+            background:transparent !important;
+        }
+
+        > div {
+            display: flex; 
+            gap: ${theme.spacing(2)};                  
+        }
+
+        > span {
+            margin-bottom: ${theme.spacing(1)};
+            display: block;
+        }              
     `
 );
 
@@ -19,7 +45,7 @@ export const CardTemplate = styled("div")(
 	() => `
           cursor: pointer;
           width: min-content;
-          transition: .3s;         
+          transition: .3s;           
     `
 );
 
@@ -37,5 +63,17 @@ export const CardContent = styled("div")(
         :hover {
             outline: 1px solid ${theme.palette.primary.main};            
         }
+    `
+);
+
+export const TemplateLinearGradient = styled("div")(
+	({ theme }) => `                    
+        width: ${theme.spacing(15)};
+        height: ${theme.spacing(29)};
+        background: linear-gradient(90deg, rgba(255, 0, 0, 0) 0%, ${theme.palette.grey[900]} 100%);
+        border-radius: ${theme.spacing(1)};
+        position: absolute;
+        top: ${theme.spacing(1)};
+        right: 1px;
     `
 );
