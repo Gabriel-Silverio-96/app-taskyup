9a19c4c2931cdc65eb887a75e2baed5bb601cf32
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-05-13 15:23:30 -0300

    refactor(notes): variable name

--- src/modules/notes/Note/CardNote/CardNote.tsx
+++ src/modules/notes/Note/CardNote/CardNote.tsx
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 import { useContextNote } from "../Context";
 import useDialogNote from "../shared/hook/useDialogNote";
 import CardNoteView from "./CardNoteView";
-import fetchNotes from "./service";
+import fetchGetNotes from "./service";
 import { TCount } from "./types/CardNote.component";
 
 const CardNote: React.FC = () => {
@@ -15,7 +15,7 @@ const CardNote: React.FC = () => {
 	const { openDialogEditNote, openDialogDeleteSingleNote } = useDialogNote();
 	
 	const queryKey = ["notes", { variable: board_id }];
-	const queryFn = () => fetchNotes(board_id);
+	const queryFn = () => fetchGetNotes(board_id);
 
 	const onSuccess = ({ count }: TCount) => setCountNotes(count);	
 	
--- src/modules/notes/Note/CardNote/service/index.ts
+++ src/modules/notes/Note/CardNote/service/index.ts
@@ -1,9 +1,9 @@
 import api from "shared/services/api";
 import { IListNotes } from "../types/CardNote.component";
 
-const fetchNotes = async (board_id: string | undefined ) => {
+const fetchGetNotes = async (board_id: string | undefined ) => {
 	const { data } = await api.get<IListNotes>(`/notes/list/board_id=${board_id}`);    
 	return data;
 };
 
-export default fetchNotes;
+export default fetchGetNotes;
