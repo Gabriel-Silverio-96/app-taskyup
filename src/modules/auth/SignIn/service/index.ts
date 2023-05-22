93b94365adeb7046b65e9f2db0da23dbe3a43777
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-05-22 20:54:53 -0300

    refactor(auth): rename service sign in password

--- src/modules/auth/SignIn/service/index.ts
+++ src/modules/auth/SignIn/service/index.ts
@@ -1,9 +1,9 @@
 import api from "shared/services/api";
 import { IFetchSignInResponse, ISignInForm } from "../types/SignIn.component";
 
-const fetchSignIn = async (form: ISignInForm) => {
+const fetchPostSignIn = async (form: ISignInForm) => {
 	const response = await api.post<IFetchSignInResponse>("auth/login", form);    
 	return response;
 };
 
-export default fetchSignIn;
+export default fetchPostSignIn;
