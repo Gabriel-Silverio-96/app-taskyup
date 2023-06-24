e183f4facb0445ef466b905dae52944d78de87cf
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-06-24 15:45:25 -0300

    refactor(account): types

---
+++ src/modules/account/Profile/ProfileForm/types/ProfileForm.types.ts
@@ -0,0 +1,23 @@
+import { FieldErrors, UseFormHandleSubmit, UseFormRegister } from "react-hook-form";
+
+export interface IFetchGetProfileResponse {
+    full_name: string;
+    email: string;
+}
+
+export interface IProfileForm {
+    full_name: string;
+    email: string;
+}
+
+export type TFetchPutProfile = IProfileForm;
+
+export interface IProfileFormView {
+    register: UseFormRegister<IFetchGetProfileResponse>;
+    isLoading: boolean;
+    errors: FieldErrors<IFetchGetProfileResponse>;
+    data: IFetchGetProfileResponse | undefined; 
+    handleSubmit: UseFormHandleSubmit<IFetchGetProfileResponse>;
+    mutate: (form: IFetchGetProfileResponse) => void;
+    isSaving: boolean;
+}
