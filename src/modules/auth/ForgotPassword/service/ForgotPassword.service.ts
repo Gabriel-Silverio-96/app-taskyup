65e4406f69d24555ac07a3b2dba84d513d0a721c
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-06-25 16:05:40 -0300

    refactor(auth): rename service

--- src/modules/auth/ForgotPassword/service/ForgotPassword.service.ts
+++ src/modules/auth/ForgotPassword/service/ForgotPassword.service.ts
@@ -2,9 +2,9 @@ import { IFetchResponseDefault } from "shared/common/types/Fetch";
 import api from "shared/services/api";
 import { IForgotPasswordForm } from "../types";
 
-const fetchForgotPassword = async (form: IForgotPasswordForm) => {
+const fetchPostForgotPasswordService = async (form: IForgotPasswordForm) => {
 	const response = await api.post<IFetchResponseDefault>("auth/forgot-password", form);
 	return response;
 };
 
-export { fetchForgotPassword };
+export { fetchPostForgotPasswordService };
