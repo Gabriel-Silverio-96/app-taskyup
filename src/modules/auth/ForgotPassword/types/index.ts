dd2683acbd265b72d120610ef59a8b96b2d8b641
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-06-25 16:18:49 -0300

    refactor(auth): types file

--- src/modules/auth/ForgotPassword/types/index.ts
+++ src/modules/auth/ForgotPassword/types/index.ts
@@ -1,20 +1,2 @@
-import { FieldErrors, UseFormRegister } from "react-hook-form";
-
-export interface IForgotPasswordForm {
-	email: string;
-}
-
-export type TFetchPostForgotPasswordService = IForgotPasswordForm;
-
-export interface ISendEmail {
-	email: string;
-	isSending: boolean;
-}
-
-export interface IForgotPasswordView {
-	register: UseFormRegister<IForgotPasswordForm>;
-	errors: FieldErrors<IForgotPasswordForm>;
-	isLoading: boolean;
-	sendEmail: ISendEmail;
-	onSubmit: () => void;
-}
+export * as default from "./ForgotPassword.types";
+export * from "./ForgotPassword.types";
