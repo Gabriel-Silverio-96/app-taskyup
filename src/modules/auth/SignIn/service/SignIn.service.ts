1e1a224e62fbd9d453a53c18c2ec00846c59f5c4
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-06-27 19:52:59 -0300

    refactor(auth): service file

---
+++ src/modules/auth/SignIn/service/SignIn.service.ts
@@ -0,0 +1,9 @@
+import api from "shared/services/api";
+import { IFetchSignInResponse, TypeFetchPostSignIn } from "../types/SignIn.component";
+
+const fetchPostSignIn = async (payload: TypeFetchPostSignIn) => {
+	const response = await api.post<IFetchSignInResponse>("auth/login", payload);    
+	return response;
+};
+
+export { fetchPostSignIn };
--- src/modules/auth/SignIn/service/index.ts
+++ src/modules/auth/SignIn/service/index.ts
@@ -1,9 +1,2 @@
-import api from "shared/services/api";
-import { IFetchSignInResponse, TypeFetchPostSignIn } from "../types/SignIn.component";
-
-const fetchPostSignIn = async (payload: TypeFetchPostSignIn) => {
-	const response = await api.post<IFetchSignInResponse>("auth/login", payload);    
-	return response;
-};
-
-export { fetchPostSignIn };
+export * as default from "./SignIn.service";
+export * from "./SignIn.service";
