1101e31879fb955adb438edc72e69b7f3ad0d734
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-07-01 18:10:15 -0300

    refactor(dashboard): interface name

--- src/modules/dashboard/Board/CardBoard/utils/select-board-icon/select-board-icon.tsx
+++ src/modules/dashboard/Board/CardBoard/utils/select-board-icon/select-board-icon.tsx
@@ -3,12 +3,12 @@ import { GoNote } from "react-icons/go";
 import { MdOutlineNotes, MdOutlineSpaceDashboard } from "react-icons/md";
 import { TypeBoard } from "modules/dashboard/Board/CardBoard/types/CardBoard.types";
 
-interface BoardIconMap {
+interface IBoardIcon {
 	[key: string]: ReactNode;
 }
 
 const selectBoardIcon = (typeBoard: TypeBoard, iconColor: string) => {
-	const boardIcon: BoardIconMap = {
+	const boardIcon: IBoardIcon = {
 		notes: <GoNote size={15} color={iconColor} />,
 		texts: <MdOutlineNotes color={iconColor} />,
 		kanban: <MdOutlineSpaceDashboard color={iconColor} />,
