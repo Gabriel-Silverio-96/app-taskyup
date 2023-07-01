0eabf6e0bb5b951fa98dbe0cb10a2b861fa13eea
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-07-01 16:53:43 -0300

    refactor(dashboard): rename service board

--- src/modules/dashboard/Board/CardBoard/service/index.ts
+++ src/modules/dashboard/Board/CardBoard/service/index.ts
@@ -1,9 +1,9 @@
 import api from "shared/services/api";
 import { IFetchBoard } from "../types/CardBoard.component";
 
-const fetchBoard = async () => {
+const fetchGetBoardService = async () => {
 	const { data } = await api.get<IFetchBoard[]>("/board");
 	return data;
 };
 
-export default fetchBoard;
+export default fetchGetBoardService;
