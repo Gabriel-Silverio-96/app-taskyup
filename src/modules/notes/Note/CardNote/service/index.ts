cfd6d076bd9123e1747dc393a79b77a43e97d873
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-07-11 20:16:43 -0300

    refactor(notes): rename service

--- src/modules/notes/Note/CardNote/service/index.ts
+++ src/modules/notes/Note/CardNote/service/index.ts
@@ -1,9 +1,9 @@
 import api from "shared/services/api";
 import { IListNotes } from "../types/CardNote.component";
 
-const fetchGetNotes = async (board_id: string | undefined ) => {
-	const { data } = await api.get<IListNotes>(`/notes/list/board_id=${board_id}`);    
+const fetchGetNotesService = async (board_id: string | undefined ) => {
+	const { data } = await api.get<IListNotes>(`/notes/list/board_id=${board_id}`);	
 	return data;
 };
 
-export default fetchGetNotes;
+export default fetchGetNotesService;
