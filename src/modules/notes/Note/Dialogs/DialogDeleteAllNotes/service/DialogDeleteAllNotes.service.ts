dfaf10aa44b4bb0388a830dafe16201e3c2a0dff
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-07-13 19:59:07 -0300

    refactor(notes): services file name

---
+++ src/modules/notes/Note/Dialogs/DialogDeleteAllNotes/service/DialogDeleteAllNotes.service.ts
@@ -0,0 +1,8 @@
+import { IFetchResponseDefault } from "shared/common/types/Fetch";
+import api from "shared/services/api";
+
+const fetchDeleteAllNotesService = async (boardID: string | undefined) => {
+	await api.delete<IFetchResponseDefault>(`notes/delete-all/board_id=${boardID}`);
+};
+
+export { fetchDeleteAllNotesService };
--- src/modules/notes/Note/Dialogs/DialogDeleteAllNotes/service/index.ts
+++ src/modules/notes/Note/Dialogs/DialogDeleteAllNotes/service/index.ts
@@ -1,8 +1,2 @@
-import { IFetchResponseDefault } from "shared/common/types/Fetch";
-import api from "shared/services/api";
-
-const fetchDeleteAllNotesService = async (boardID: string | undefined) => {
-	await api.delete<IFetchResponseDefault>(`notes/delete-all/board_id=${boardID}`);
-};
-
-export { fetchDeleteAllNotesService };
+export * as default from "./DialogDeleteAllNotes.service";
+export * from "./DialogDeleteAllNotes.service";
