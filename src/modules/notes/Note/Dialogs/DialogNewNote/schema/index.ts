fe5359d1c0ee7f5242356d3150541ec3d95f13b8
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-07-22 12:52:32 -0300

    refactor(notes): remove components dialog edit and create note

--- src/modules/notes/Note/Dialogs/DialogNewNote/schema/index.ts
+++
@@ -1,9 +0,0 @@
-import * as Yup from "yup";
-
-const schema = Yup.object({
-	title_note: Yup.string().required("Title note is a required field")
-		.min(3, "Should be 3 chars minimum"),
-	observation: Yup.string().max(500, "Max of 500 characters")
-});
-
-export default schema;
