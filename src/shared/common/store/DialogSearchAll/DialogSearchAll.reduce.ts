1282663f44b31e060f70e18cb5286f64426cb233
gabriel-silverio-96 <gabriel.infoak2@gmail.com>
2023-07-25 06:05:03 -0300

    refactor(shared): types

--- src/shared/common/store/DialogSearchAll/DialogSearchAll.reduce.ts
+++ src/shared/common/store/DialogSearchAll/DialogSearchAll.reduce.ts
@@ -1,11 +1,13 @@
+import { IDialogSearchAllAction, IDialogSearchAllState } from "./types/DialogSearchAll.types";
+
 export const OPEN_DIALOG_SEARCH_ALL_TYPE = "OPEN_DIALOG_SEARCH_ALL_TYPE";
 export const CLOSE_DIALOG_SEARCH_ALL_TYPE = "CLOSE_DIALOG_SEARCH_ALL_TYPE";
 
-const INITIAL_STATE: any = {
+const INITIAL_STATE: IDialogSearchAllState = {
 	open: false,
 };
 
-export default function (state = INITIAL_STATE, action: any) {
+export default function (state = INITIAL_STATE, action: IDialogSearchAllAction) {
 	const { payload, type } = action;
 	switch (type) {
 	case OPEN_DIALOG_SEARCH_ALL_TYPE:
--- src/shared/components/Drawer/components/DialogSearchAll/DialogSearchAll.tsx
+++ src/shared/components/Drawer/components/DialogSearchAll/DialogSearchAll.tsx
@@ -3,10 +3,11 @@ import DialogSearchAllView from "./DialogSearchAllView";
 import { useDispatch, useSelector } from "react-redux";
 import { createAction } from "shared/common/store/store.action";
 import { CLOSE_DIALOG_SEARCH_ALL_TYPE } from "shared/common/store/DialogSearchAll/DialogSearchAll.reduce";
+import { IDialogSearchAllState } from "shared/common/store/DialogSearchAll/types/DialogSearchAll.types";
 
 const DialogSearchAll: React.FC = () => {    
 	const dispatch = useDispatch();
-	const dialogSearchAll = useSelector((state: { dialogSearchAll: any }) => state.dialogSearchAll);	
+	const dialogSearchAll = useSelector((state: { dialogSearchAll: IDialogSearchAllState }) => state.dialogSearchAll);	
     
 	const handleClickCloseDialogSearchAll = () => dispatch(createAction(CLOSE_DIALOG_SEARCH_ALL_TYPE));
 
