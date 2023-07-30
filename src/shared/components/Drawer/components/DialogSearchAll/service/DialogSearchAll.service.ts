a0dea84b16235bdef309b6e4c0bfda9ef8189764
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-07-30 13:47:45 -0300

    feat(shared): set pageNumber to zero

--- src/shared/components/Drawer/components/DialogSearchAll/service/DialogSearchAll.service.ts
+++ src/shared/components/Drawer/components/DialogSearchAll/service/DialogSearchAll.service.ts
@@ -2,7 +2,7 @@ import api from "shared/services/api";
 import { IFetchGetSearchAllResponse, IFetchGetSearchAllService } from "../types/DialogSearchAll.types";
 
 const fetchGetSearchAllService = async (payload: IFetchGetSearchAllService) => {
-	const { query, pageSize = 10, pageNumber = 1 } = payload;
+	const { query, pageSize = 10, pageNumber = 0 } = payload;
 	const config = { params: { query, pageSize, pageNumber } };
     
 	const { data } = await api.get<IFetchGetSearchAllResponse>("search/all", config);
