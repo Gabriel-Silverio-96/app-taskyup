246c504ebb5a7bdea85169a999d968bd1529133f
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-07-31 18:24:33 -0300

    refactor(texts): name parameter

--- src/modules/texts/Text/Dialogs/DialogDeleteAllTexts/service/DialogDeleteAllTexts.service.ts
+++ src/modules/texts/Text/Dialogs/DialogDeleteAllTexts/service/DialogDeleteAllTexts.service.ts
@@ -1,8 +1,8 @@
 import { IFetchResponseDefault } from "shared/common/types/Fetch";
 import api from "shared/services/api";
 
-const fetchDeleteAllTextsService = async (boardID: string | undefined) => {
-	const { data } = await api.delete<IFetchResponseDefault>(`/text/delete-all/board_id=${boardID}`);
+const fetchDeleteAllTextsService = async (board_id: string | undefined) => {
+	const { data } = await api.delete<IFetchResponseDefault>(`/text/delete-all/board_id=${board_id}`);
 	return data;
 };
 
