83e1a6e114a8d8a8fd1692a646996c8cda914bea
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-08-01 17:33:31 -0300

    refactor(texts): file service

--- src/modules/texts/Text/Dialogs/DialogDeleteSingleText/service/index.ts
+++ src/modules/texts/Text/Dialogs/DialogDeleteSingleText/service/index.ts
@@ -1,9 +1,2 @@
-import { IFetchResponseDefault } from "shared/common/types/Fetch";
-import api from "shared/services/api";
-
-const fetchDeleteOneTextService = async (text_id: string) => {
-	const { data } = await api.delete<IFetchResponseDefault>(`/text/delete-this/text_id=${text_id}`);
-	return data;
-};
-
-export { fetchDeleteOneTextService };
+export * as default from "./DialogDeleteOneText.service";
+export * from "./DialogDeleteOneText.service";
