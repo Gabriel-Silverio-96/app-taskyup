9c3431ea81aaaa0bc4de31b47ff5262d172e177f
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-08-01 17:36:01 -0300

    refactor(texts): absolute imports

--- src/modules/texts/Text/Dialogs/DialogDeleteSingleText/DialogDeleteSingleText.tsx
+++ src/modules/texts/Text/Dialogs/DialogDeleteSingleText/DialogDeleteSingleText.tsx
@@ -1,6 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import React, { memo } from "react";
-import { useContextText } from "../../Context";
+import { useContextText } from "modules/texts/Text/Context";
 import useDialogText from "../../hooks/useDialogText";
 import DialogDeleteSingleTextView from "./DialogDeleteSingleTextView";
 import { fetchDeleteOneTextService } from "./service";
