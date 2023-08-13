a6d16957118c996f6de8af01b1c6e3e545490c8d
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-08-13 10:59:46 -0300

    refactor(texts): file service

--- src/modules/texts/TextEdit/Markdown/service/index.ts
+++ src/modules/texts/TextEdit/Markdown/service/index.ts
@@ -1,24 +1,2 @@
-import { IFetchResponseDefault } from "shared/common/types/Fetch";
-import api from "shared/services/api";
-import { IFetchEditText, IFetchText, TFetchParams } from "./types";
-
-const fetchGetOneTextService = async (text_id: TFetchParams) => {
-	const { data } = await api.get<IFetchText>(`/text/text_id=${text_id}`);
-	return data;
-};
-
-const fetchPatchTextService = async ({
-	board_id,
-	text_id,
-	data,
-}: IFetchEditText) => {
-	const params = { params: { board_id, text_id } };
-	const { data: response } = await api.patch<IFetchResponseDefault>(
-		"/text/edit",
-		{ ...data },
-		params
-	);
-	return response;
-};
-
-export { fetchGetOneTextService, fetchPatchTextService };
+export * as default from "./Markdown.service";
+export * from "./Markdown.service";
