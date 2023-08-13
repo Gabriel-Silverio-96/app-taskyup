3e96052fe0add1c0c65a326c8118535fa6af9583
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-08-13 11:27:23 -0300

    refactor(texts): rename file types

--- src/modules/texts/TextEdit/Markdown/HeaderText/HeaderText.tsx
+++ src/modules/texts/TextEdit/Markdown/HeaderText/HeaderText.tsx
@@ -1,16 +1,22 @@
 import React, { memo } from "react";
 import { useSearchParams } from "react-router-dom";
 import HeaderTextView from "./HeaderTextView";
-import { IHeaderText } from "./types/HeaderText.component";
+import { IHeaderText } from "./types";
 
-const HeaderText: React.FC<IHeaderText> = ({ saveText, titleBoard, titleText, onChangeTextTitle }) => {	
-	const [ searchParams ] = useSearchParams();
+const HeaderText: React.FC<IHeaderText> = ({
+	saveText,
+	titleBoard,
+	titleText,
+	onChangeTextTitle,
+}) => {
+	const [searchParams] = useSearchParams();
 	const board_id = searchParams.get("board_id");
 
 	const linkPreviousPage = `/texts/${board_id}`;
 
 	const disableButtonSave = titleText.length < 3;
-	const handleMessageError = titleText.length < 3 && "Minimum of 3 characters";
+	const handleMessageError =
+		titleText.length < 3 && "Minimum of 3 characters";
 
 	return (
 		<HeaderTextView
--- src/modules/texts/TextEdit/Markdown/HeaderText/HeaderTextView.tsx
+++ src/modules/texts/TextEdit/Markdown/HeaderText/HeaderTextView.tsx
@@ -3,21 +3,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "shared/components/Logo";
 import { Nav, TextTitle, TextField, Breadcrumbs } from "./style";
-import {  IHeaderTextView } from "./types/HeaderText.component";
+import { IHeaderTextView } from "./types";
 
-const HeaderTextView: React.FC<IHeaderTextView> = (props) => {
-	const { 
-		saveText, 
-		titleBoard, 
-		titleText, 
-		onChangeTextTitle, 
-		linkPreviousPage, 
-		disableButtonSave, 
-		handleMessageError 
-	} = props;	
+const HeaderTextView: React.FC<IHeaderTextView> = props => {
+	const {
+		saveText,
+		titleBoard,
+		titleText,
+		onChangeTextTitle,
+		linkPreviousPage,
+		disableButtonSave,
+		handleMessageError,
+	} = props;
 
 	return (
-		<Grid container alignItems="center" justifyContent="space-between" sx={{ mb: 3 }}>
+		<Grid
+			container
+			alignItems="center"
+			justifyContent="space-between"
+			sx={{ mb: 3 }}>
 			<Grid item md={3}>
 				<Link to="/dashboard">
 					<Logo size={100} />
@@ -27,19 +31,25 @@ const HeaderTextView: React.FC<IHeaderTextView> = (props) => {
 				<Breadcrumbs>
 					<Typography variant="caption">{titleBoard} /</Typography>
 					<TextTitle>
-						<TextField value={titleText} onChange={onChangeTextTitle} />
-						<Typography variant="caption">{handleMessageError}</Typography>
+						<TextField
+							value={titleText}
+							onChange={onChangeTextTitle}
+						/>
+						<Typography variant="caption">
+							{handleMessageError}
+						</Typography>
 					</TextTitle>
 				</Breadcrumbs>
 			</Grid>
 			<Grid item md={3}>
 				<Nav>
 					<Link to={linkPreviousPage}>
-						<Button	variant="text">
-							Back
-						</Button>
+						<Button variant="text">Back</Button>
 					</Link>
-					<Button	variant="contained"	onClick={saveText} disabled={disableButtonSave}>
+					<Button
+						variant="contained"
+						onClick={saveText}
+						disabled={disableButtonSave}>
 						Save
 					</Button>
 				</Nav>
