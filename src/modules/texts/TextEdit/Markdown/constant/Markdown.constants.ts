ec6a6dfafdd82924a4aff5fe7f6b9e8a61151dba
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-08-15 18:20:44 -0300

    refactor(texts): file constants

---
+++ src/modules/texts/TextEdit/Markdown/constant/Markdown.constants.ts
@@ -0,0 +1,14 @@
+import frontmatter from "@bytemd/plugin-frontmatter";
+import gemoji from "@bytemd/plugin-gemoji";
+import gfm from "@bytemd/plugin-gfm";
+import highlight from "@bytemd/plugin-highlight";
+import mermaid from "@bytemd/plugin-mermaid";
+
+export const INITIAL_STATE_DATA = {
+	title_board: "",
+	title_text: "",
+	text: "",
+	created_at: "",
+};
+
+export const PLUGINS = [highlight(), frontmatter(), gfm(), mermaid(), gemoji()];
