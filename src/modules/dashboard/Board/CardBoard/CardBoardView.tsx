4857b7ae6d280b204e752db2d3c9e0dee0cea2c0
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-08-28 18:26:44 -0300

    feat(dashboard): interface member board

--- src/modules/dashboard/Board/CardBoard/CardBoardView.tsx
+++ src/modules/dashboard/Board/CardBoard/CardBoardView.tsx
@@ -4,14 +4,20 @@ import { AiOutlineDelete } from "react-icons/ai";
 import { BsThreeDots } from "react-icons/bs";
 import { FiEdit } from "react-icons/fi";
 import { Link } from "react-router-dom";
-import Loading from "shared/components/Loading";
-import { CardBoardContainer, CardContent, CardHeader, CardIcon, Menu } from "./style";
-import { ICardBoardView } from "./types/CardBoard.types";
 import EmptyBoard from "shared/components/EmptyBoard/EmptyBoard";
+import Loading from "shared/components/Loading";
+import {
+	CardBoardContainer,
+	CardContent,
+	CardHeader,
+	CardIcon,
+	Menu,
+} from "./style";
+import { ICardBoardView } from "./types/CardBoard.types";
 import { selectBoardIcon } from "./utils/select-board-icon";
 
 const CardBoardView: React.FC<ICardBoardView> = props => {
-	const { 
+	const {
 		board,
 		isFetching,
 		palette,
@@ -21,82 +27,116 @@ const CardBoardView: React.FC<ICardBoardView> = props => {
 		isOpenMenu,
 		handleBoardID,
 		openDialogEditBoard,
-		openDialogDeleteSingleBoard
-	 } = props;
-			
+		openDialogDeleteSingleBoard,
+	} = props;
+
 	return (
 		<Grid container spacing={2}>
 			<Loading isLoading={isFetching} backdrop />
-			<EmptyBoard 
-				show={board?.length === 0} 
-				title="You have not created any board" 
-				message="Create a board 😊" 
+			<EmptyBoard
+				show={board?.length === 0}
+				title="You have not created any board"
+				message="Create a board 😊"
 			/>
 
 			{board &&
-				board.map(({ title, type_board, board_id, background_image }) => {													
-					const boardIcon = selectBoardIcon(type_board, palette.secondary.main);					
-					const linkBoard = `/${type_board}/${board_id}`;
+				board.map(
+					({ title, type_board, board_id, background_image }) => {
+						const boardIcon = selectBoardIcon(
+							type_board,
+							palette.secondary.main
+						);
+						const linkBoard = `/${type_board}/${board_id}`;
 
-					return (
-						<Grid item xl={2} md={3} xs={12} key={board_id}>
-							<CardBoardContainer backgroundimage={background_image}>
-								<Card sx={{ height: 120 }}>
-									<CardContent>
-										<CardHeader>
+						return (
+							<Grid item xl={2} md={3} xs={12} key={board_id}>
+								<CardBoardContainer
+									backgroundimage={background_image}>
+									<Card sx={{ height: 120 }}>
+										<CardContent>
+											<CardHeader>
+												<Link to={linkBoard}>
+													<CardIcon>
+														{boardIcon}
+														<Typography
+															color="text.secondary"
+															gutterBottom
+															sx={{
+																fontSize: 12,
+															}}>
+															{type_board}
+														</Typography>
+													</CardIcon>
+												</Link>
+												<IconButton
+													sx={{ p: 0 }}
+													disabled={isFetching}
+													onClick={(
+														event: MouseEvent<HTMLButtonElement>
+													) => {
+														handleBoardID(board_id);
+														openMenu(event);
+													}}
+													data-testid="button-card-board-options">
+													<BsThreeDots />
+												</IconButton>
+												<Menu
+													anchorEl={anchorEl}
+													open={isOpenMenu}
+													onClose={closeMenu}
+													autoFocus={false}
+													transitionDuration={{
+														appear: 0,
+														enter: 0,
+														exit: 0,
+													}}
+													anchorOrigin={{
+														vertical: "bottom",
+														horizontal: "right",
+													}}
+													transformOrigin={{
+														vertical: "top",
+														horizontal: "right",
+													}}>
+													<MenuItem
+														onClick={() =>
+															openDialogEditBoard(
+																closeMenu
+															)
+														}>
+														<FiEdit /> Edit
+													</MenuItem>
+													<MenuItem
+														onClick={() =>
+															openDialogDeleteSingleBoard(
+																closeMenu
+															)
+														}
+														sx={{
+															color: palette.error
+																.main,
+														}}>
+														<AiOutlineDelete />{" "}
+														Delete
+													</MenuItem>
+												</Menu>
+											</CardHeader>
 											<Link to={linkBoard}>
-												<CardIcon>
-													{boardIcon}
-													<Typography color="text.secondary" gutterBottom sx={{ fontSize: 12 }}>
-														{type_board}
-													</Typography>
-												</CardIcon>
+												<Typography
+													variant="h6"
+													fontWeight={800}
+													fontSize={16}
+													data-testid="card-board-title">
+													{title}
+												</Typography>
 											</Link>
-											<IconButton sx={ { p: 0 } } 
-												disabled={isFetching}
-												onClick={(event: MouseEvent<HTMLButtonElement>) => {
-													handleBoardID(board_id);
-													openMenu(event);
-												}}
-												data-testid="button-card-board-options"
-											>
-												<BsThreeDots />
-											</IconButton>
-											<Menu 
-												anchorEl={anchorEl}
-												open={isOpenMenu}
-												onClose={closeMenu}											
-												autoFocus={false}
-												transitionDuration={{ appear: 0, enter: 0, exit: 0 }}
-												anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-												transformOrigin={{ vertical: "top", horizontal: "right" }}
-											>
-												<MenuItem onClick={() => openDialogEditBoard(closeMenu)}>
-													<FiEdit /> Edit
-												</MenuItem>
-												<MenuItem onClick={() => openDialogDeleteSingleBoard(closeMenu)} 
-													sx={ { color: palette.error.main } }
-												>
-													<AiOutlineDelete /> Delete
-												</MenuItem>
-											</Menu>
-										</CardHeader>
-										<Link to={linkBoard}>
-											<Typography
-												variant="h6"												
-												fontWeight={800}
-												fontSize={16}
-												data-testid="card-board-title"
-											>
-												{title}
-											</Typography>
-										</Link>
-									</CardContent>
-								</Card>
-							</CardBoardContainer>
-						</Grid>
-					);
-				})}			
+										</CardContent>
+									</Card>
+								</CardBoardContainer>
+							</Grid>
+						);
+					}
+				)}
 		</Grid>
 	);
 };
--- src/modules/dashboard/Board/CardBoard/types/CardBoard.types.ts
+++ src/modules/dashboard/Board/CardBoard/types/CardBoard.types.ts
@@ -4,13 +4,17 @@ import { FunctionNonValueReturning } from "shared/types";
 
 export type TypeBoard = "notes" | "texts" | "kanban";
 
+export interface IMemberBoard {
+	first_name: string;
+}
+
 export interface IFetchGetBoardResponse {
 	board_id: string;
 	title: string;
 	type_board: TypeBoard;
 	created_at: string;
 	background_image: string;
-	members_board: string[];
+	members_board: Array<IMemberBoard>;
 }
 
 export interface ICardBoardView {
