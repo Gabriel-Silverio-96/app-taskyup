48ec32063bed12e91c91bc9aa0e99a259bb7f821
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-08-30 19:05:04 -0300

    refactor(dashboard): rename prop

--- src/modules/dashboard/Board/CardBoard/CardBoardView.tsx
+++ src/modules/dashboard/Board/CardBoard/CardBoardView.tsx
@@ -58,7 +58,7 @@ const CardBoardView: React.FC<ICardBoardView> = props => {
 						return (
 							<Grid item xl={2} md={3} xs={12} key={board_id}>
 								<CardBoardContainer
-									backgroundimage={background_image}>
+									backgroundImage={background_image}>
 									<Card sx={{ height: 120 }}>
 										<CardContent>
 											<CardHeader>
