23b63de019241e7c2e6282d29408f7e380282577
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-09-05 18:47:08 -0300

    refactor(notes): context name

---
+++ src/modules/notes/Note/Context/Note.context.tsx
@@ -0,0 +1,47 @@
+import { createContext, useContext, useState } from "react";
+import { IContextNote, IContextProviderNote } from "./types";
+
+export const ContextNote = createContext<IContextNote | undefined>(undefined);
+
+export const ContextProviderNote: React.FC<IContextProviderNote> = ({
+	children,
+}) => {
+	const [isOpenDialogNewNote, setIsOpenDialogNewNote] = useState(false);
+	const [isOpenDialogEditNote, setIsOpenDialogEditNote] = useState(false);
+	const [isOpenDialogDeleteSingleNote, setIsOpenDialogDeleteSingleNote] =
+		useState(false);
+	const [isOpenDialogDeleteAllNotes, setIsOpenDialogDeleteAllNotes] =
+		useState(false);
+	const [noteID, setNoteID] = useState("");
+	const [countNotes, setCountNotes] = useState(0);
+
+	const value = {
+		isOpenDialogNewNote,
+		setIsOpenDialogNewNote,
+		isOpenDialogEditNote,
+		setIsOpenDialogEditNote,
+		isOpenDialogDeleteSingleNote,
+		setIsOpenDialogDeleteSingleNote,
+		isOpenDialogDeleteAllNotes,
+		setIsOpenDialogDeleteAllNotes,
+		countNotes,
+		setCountNotes,
+		noteID,
+		setNoteID,
+	};
+
+	return (
+		<ContextNote.Provider value={value}>{children}</ContextNote.Provider>
+	);
+};
+
+export const useContextNote = () => {
+	const context = useContext(ContextNote);
+	if (context === undefined) {
+		throw new Error(
+			"useContextNote must be used within a ContextProviderNote"
+		);
+	}
+
+	return context;
+};
---
+++ src/modules/notes/Note/Context/types/index.ts
@@ -0,0 +1,2 @@
+export * as default from "./Note.context.types";
+export * from "./Note.context.types";
