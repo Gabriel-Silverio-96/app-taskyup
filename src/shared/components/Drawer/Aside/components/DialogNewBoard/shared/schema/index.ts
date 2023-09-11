26cd41104afc99b860e16d3675e6f4561bade2b6
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-09-11 16:36:02 -0300

    refactor(shared): move dialog to components folder

---
+++ src/shared/components/Drawer/Aside/components/DialogNewBoard/shared/schema/index.ts
@@ -0,0 +1,14 @@
+import * as Yup from "yup";
+
+const BOARD_TYPE_ID = [
+	"1f819d7d-11b5-4742-be86-2286ef50b5d5",
+	"43fa2ae7-48eb-4e1c-a746-617a643fee45",
+	"1cf4ff82-59bd-4733-890c-c9226ea0df2f",
+];
+
+const schema = Yup.object({
+	title: Yup.string().required().min(3, "Should be 3 chars minimum"),
+	board_type_id: Yup.string().required("Required field").oneOf(BOARD_TYPE_ID),
+});
+
+export default schema;
