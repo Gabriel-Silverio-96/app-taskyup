35c66024e95cd904892724afefd4dd30065bc253
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-09-13 09:31:19 -0300

    feat(shared): validation first name

--- src/shared/common/validation/index.ts
+++ src/shared/common/validation/index.ts
@@ -1,3 +1,8 @@
+const FIRST_NAME_DEFAULT_VALIDATION = {
+	minLength: 2,
+	messageFirstName: "First name is a required field. Min 2 characters"
+};
+
 const FULL_NAME_DEFAULT_VALIDATION = {
 	regexFullName: /^([\wáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]{2,})+\s+([\wáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ\s]{2,})+$/i,
 	messageFullName: "Full name is a required field"
@@ -8,4 +13,4 @@ const PASSWORD_DEFAULT_VALIDATION = {
 	messagePassword: "Min 8 characters at least two letter two number \n and one character special"
 };
 
-export { FULL_NAME_DEFAULT_VALIDATION, PASSWORD_DEFAULT_VALIDATION };
+export { FULL_NAME_DEFAULT_VALIDATION, PASSWORD_DEFAULT_VALIDATION, FIRST_NAME_DEFAULT_VALIDATION };
