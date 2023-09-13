42611a3118ce81850d99ea9fdbc71691e74a9f46
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-09-13 10:07:00 -0300

    style(auth): title break line

--- src/modules/auth/SignUp/SignUpView.tsx
+++ src/modules/auth/SignUp/SignUpView.tsx
@@ -7,31 +7,35 @@ import TextFieldPassword from "shared/components/TextFieldPassword";
 import { BackgroundSignUp, Footer, GridForm } from "./styles";
 import { ISignUpView } from "./types/SignUp.types";
 
-const SignUpView: React.FC<ISignUpView> = (props) => {
+const SignUpView: React.FC<ISignUpView> = props => {
 	const { register, errors, handleSubmit, signUpSubmit, isLoading } = props;
 	return (
-		<Grid container justifyContent="space-around" sx={{ minHeight: "100vh" }}>
+		<Grid
+			container
+			justifyContent="space-around"
+			sx={{ minHeight: "100vh" }}>
 			<Grid item md={6} sm={12}>
 				<BackgroundSignUp>
-					<Typography variant="h3" fontWeight={700}>Time to create your account</Typography>
-					<p>You are a few steps to improve your organization</p>
+					<div>
+						<Typography variant="h3" fontWeight={700}>
+							Time to create <br />
+							your account
+						</Typography>
+						<p>You are a few steps to improve your organization</p>
+					</div>
 				</BackgroundSignUp>
 			</Grid>
-			<Grid 
-				item 
+			<Grid
+				item
 				md={6}
 				sm={12}
 				justifyContent="center"
 				display="flex"
 				alignItems="center"
 				flexDirection="column"
-				sx={{ minWidth: "20rem", marginTop: "2rem" }}
-			>
+				sx={{ minWidth: "20rem", marginTop: "2rem" }}>
 				<GridForm>
-					<Greeting 
-						title="Sign up"
-						subtitle="Fill the form ✏️"
-					/>
+					<Greeting title="Sign up" subtitle="Fill the form ✏️" />
 					<form onSubmit={handleSubmit(signUpSubmit)}>
 						<Grid container direction="column" spacing={5}>
 							<Grid item>
@@ -40,8 +44,15 @@ const SignUpView: React.FC<ISignUpView> = (props) => {
 									size="small"
 									fullWidth
 									{...register("full_name")}
-									error={errors.full_name && Boolean(errors.full_name)}
-									helperText={errors.full_name ? errors.full_name?.message : ""}
+									error={
+										errors.full_name &&
+										Boolean(errors.full_name)
+									}
+									helperText={
+										errors.full_name
+											? errors.full_name?.message
+											: ""
+									}
 									autoComplete="off"
 								/>
 							</Grid>
@@ -51,21 +62,26 @@ const SignUpView: React.FC<ISignUpView> = (props) => {
 									size="small"
 									fullWidth
 									{...register("email")}
-									error={errors.email && Boolean(errors.email)}
-									helperText={errors.email ? errors.email?.message : ""}
+									error={
+										errors.email && Boolean(errors.email)
+									}
+									helperText={
+										errors.email
+											? errors.email?.message
+											: ""
+									}
 									autoComplete="off"
 								/>
 							</Grid>
 							<Grid item>
-								<TextFieldPassword {...{ register, errors }}/>
+								<TextFieldPassword {...{ register, errors }} />
 							</Grid>
 							<Grid item>
 								<LoadingButton
 									fullWidth
 									variant="contained"
 									type="submit"
-									loading={isLoading}
-								>
+									loading={isLoading}>
 									Sign up
 								</LoadingButton>
 							</Grid>
@@ -74,9 +90,16 @@ const SignUpView: React.FC<ISignUpView> = (props) => {
 				</GridForm>
 				<Footer>
 					<Grid container direction="column">
-						<Grid display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+						<Grid
+							display="flex"
+							justifyContent="center"
+							alignItems="center"
+							flexDirection="column">
 							<Link to="/auth/signin">
-								<Typography variant="body1" textAlign="center" sx={{ mt: 3, mb: 3 }}>
+								<Typography
+									variant="body1"
+									textAlign="center"
+									sx={{ mt: 3, mb: 3 }}>
 									Do you have an account? Login
 								</Typography>
 							</Link>
