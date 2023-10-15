341c21d9abfcd7c345673a80d0653135b680e58d
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-10-15 10:20:31 -0300

    feat(texts): create context

--- src/modules/texts/TextEdit/TextEditView.tsx
+++ src/modules/texts/TextEdit/TextEditView.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Markdown from "./components/Markdown";
+import { ContextProviderTextEdit } from "./Context";
 
 const TextEditView: React.FC = () => {
 	return (
-		<>
+		<ContextProviderTextEdit>
 			<Markdown />
-		</>
+		</ContextProviderTextEdit>
 	);
 };
 
