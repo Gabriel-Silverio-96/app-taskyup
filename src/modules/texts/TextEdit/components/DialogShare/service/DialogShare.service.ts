8877ac8720c27e8b7a50b36b43f398176fc9a9ff
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-10-18 18:05:52 -0300

    feat(texts): service edit permissions

--- src/modules/texts/TextEdit/components/DialogShare/service/DialogShare.service.ts
+++ src/modules/texts/TextEdit/components/DialogShare/service/DialogShare.service.ts
@@ -4,3 +4,10 @@ export const fetchGetTextPermissions = async ({ params }: any) => {
 	const { data } = await api.get("/text/permissions", { params });
 	return data;
 };
+
+export const fetchPatchTextPermissions = async (data: any) => {
+	const { payload, board_id, text_id } = data;
+	const config = { params: { board_id, text_id } };
+
+	return await api.patch("/text/edit/permissions", payload, config);
+};
