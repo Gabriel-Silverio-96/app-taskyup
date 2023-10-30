002217e2d49498c2c7dc7f8176bd2f142aec8ce7
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-10-30 17:22:06 -0300

    refactor: function returns void

--- src/modules/texts/Text/components/Dialogs/DialogDeleteAllTexts/types/DialogDeleteAllTexts.types.ts
+++ src/modules/texts/Text/components/Dialogs/DialogDeleteAllTexts/types/DialogDeleteAllTexts.types.ts
@@ -1,7 +1,7 @@
 import { FunctionReturnsVoid } from "shared/common/types/AppTypes";
 
 export interface IDialogDeleteAllTextsView {
-	dialogDeleteAllText: { open: boolean; boardID: string };
+	dialogDeleteAllText: { open: boolean, boardID: string };
 	closeDialogDeleteAllTexts: FunctionReturnsVoid;
 	fetchDeleteAll: FunctionReturnsVoid;
 	isDeleting: boolean;
