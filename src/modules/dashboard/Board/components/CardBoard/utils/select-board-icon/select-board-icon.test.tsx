7e1766166f71f0491d880b897b27afdd55234b2e
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-11-05 10:27:54 -0300

    refactor(dashboard): card icon size

--- src/modules/dashboard/Board/components/CardBoard/utils/select-board-icon/select-board-icon.test.tsx
+++ src/modules/dashboard/Board/components/CardBoard/utils/select-board-icon/select-board-icon.test.tsx
@@ -2,40 +2,42 @@ import { GoNote } from "react-icons/go";
 import { MdOutlineNotes, MdOutlineSpaceDashboard } from "react-icons/md";
 import { selectBoardIcon } from "./select-board-icon";
 import { faker } from "@faker-js/faker";
+import { ICON_SIZE } from "shared/constants";
 
 describe("Function selectBoardIcon()", () => {
 	test("Should return the correct icon for notes", () => {
 		const iconColor = faker.color.human();
 		const typeBoard = "notes";
-		const expectedIcon = <GoNote size={15} color={iconColor} />;
-  
-		const result = selectBoardIcon(typeBoard, iconColor);  
+		const expectedIcon = (
+			<GoNote size={ICON_SIZE.MEDIUM} color={iconColor} />
+		);
+
+		const result = selectBoardIcon(typeBoard, iconColor);
 		expect(result).toEqual(expectedIcon);
 	});
-  
+
 	test("Should return the correct icon for texts", () => {
 		const iconColor = faker.color.human();
 		const typeBoard = "texts";
-		const expectedIcon = <MdOutlineNotes color={iconColor} />;
-  
-		const result = selectBoardIcon(typeBoard, iconColor);  
+		const expectedIcon = (
+			<MdOutlineNotes size={ICON_SIZE.MEDIUM} color={iconColor} />
+		);
+
+		const result = selectBoardIcon(typeBoard, iconColor);
 		expect(result).toEqual(expectedIcon);
 	});
-  
+
 	test("Should return the correct icon for kanban", () => {
 		const iconColor = faker.color.human();
 		const typeBoard = "kanban";
-		const expectedIcon = <MdOutlineSpaceDashboard color={iconColor} />;
-  
-		const result = selectBoardIcon(typeBoard, iconColor);  
+		const expectedIcon = (
+			<MdOutlineSpaceDashboard
+				size={ICON_SIZE.MEDIUM}
+				color={iconColor}
+			/>
+		);
+
+		const result = selectBoardIcon(typeBoard, iconColor);
 		expect(result).toEqual(expectedIcon);
 	});
 });
-  
-  
-  
-  
-  
-  
-  
-  
