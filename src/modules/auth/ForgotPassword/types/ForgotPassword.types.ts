75649122d16a834d9e3962da51abd2351a786fd0
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-11-09 13:19:57 -0300

    refactor(auth): remove interface

--- src/modules/auth/ForgotPassword/types/ForgotPassword.types.ts
+++ src/modules/auth/ForgotPassword/types/ForgotPassword.types.ts
@@ -5,8 +5,6 @@ export interface IForgotPasswordForm {
 	email: string;
 }
 
-export type TFetchPostForgotPasswordService = IForgotPasswordForm;
-
 export interface ISendEmail {
 	email: string;
 	isSending: boolean;
