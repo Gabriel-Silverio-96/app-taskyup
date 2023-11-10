1fd9c359fa48f0e6e6ce095d4ac1ef22f4fd11f2
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-11-10 12:24:10 -0300

    refactor(auth): use hook

--- src/modules/auth/SignIn/SignIn.tsx
+++ src/modules/auth/SignIn/SignIn.tsx
@@ -1,15 +1,15 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { SIGNIN_TYPE } from "shared/common/store/Auth/Auth.reducer";
 import { createAction } from "shared/common/store/store.action";
 import api from "shared/services/api";
-import schema from "./schema";
 import SignInView from "./SignInView";
-import { ISignInForm } from "./types";
+import schema from "./schema";
 import { fetchPostSignInService } from "./service";
+import { ISignInForm } from "./types";
 
 const SignIn: React.FC = () => {
 	const {
@@ -23,37 +23,32 @@ const SignIn: React.FC = () => {
 
 	const [isLoading, setIsLoading] = useState(false);
 
-	const signInSubmit = useCallback(
-		async ({ email, password }: ISignInForm) => {
-			try {
-				setIsLoading(true);
-				const { data } = await fetchPostSignInService({
-					body: { email, password },
-				});
-				const { token, user_data } = data;
+	const signInSubmit = async ({ email, password }: ISignInForm) => {
+		try {
+			setIsLoading(true);
+			const { data } = await fetchPostSignInService({
+				body: { email, password },
+			});
+			const { token, user_data } = data;
 
-				localStorage.setItem("@taskyup.token", token);
-				localStorage.setItem(
-					"@taskyup.user_data",
-					JSON.stringify(user_data)
-				);
-				api.defaults.headers.common[
-					"Authorization"
-				] = `Bearer ${token}`;
+			localStorage.setItem("@taskyup.token", token);
+			localStorage.setItem(
+				"@taskyup.user_data",
+				JSON.stringify(user_data)
+			);
+			api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-				const action = createAction(SIGNIN_TYPE, {
-					isAuthenticated: true,
-					user_data,
-				});
-				dispatch(action);
+			const action = createAction(SIGNIN_TYPE, {
+				isAuthenticated: true,
+				user_data,
+			});
+			dispatch(action);
 
-				return navigate("/dashboard");
-			} catch (error) {
-				setIsLoading(false);
-			}
-		},
-		[]
-	);
+			return navigate("/dashboard");
+		} catch (error) {
+			setIsLoading(false);
+		}
+	};
 
 	return (
 		<SignInView
