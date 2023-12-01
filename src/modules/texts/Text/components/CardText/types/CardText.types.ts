d0df419f445e97976b0c621e8bee08a66d841054
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-12-01 05:44:56 -0300

    refactor(texts): import interface

--- src/modules/texts/Text/components/CardText/types/CardText.types.ts
+++ src/modules/texts/Text/components/CardText/types/CardText.types.ts
@@ -1,4 +1,4 @@
-import { IFetchGetAllTextsResponse } from "modules/texts/Text/components/CardText/services/types";
+import { IFetchGetAllTextsResponse } from "modules/texts/Text/services/types";
 
 export interface ICardText {
 	data: IFetchGetAllTextsResponse | undefined;
