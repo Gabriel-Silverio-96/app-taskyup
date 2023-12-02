da1cabf4a3572323381aa12f0b409a9d64495b21
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-12-02 08:19:27 -0300

    feat: validation max characters title board

--- src/modules/dashboard/Board/components/Dialogs/DialogEditBoard/schema/DialogEditBoard.schema.ts
+++ src/modules/dashboard/Board/components/Dialogs/DialogEditBoard/schema/DialogEditBoard.schema.ts
@@ -1,5 +1,8 @@
 import * as Yup from "yup";
 
 export const DialogEditBoardSchema = Yup.object({
-	title: Yup.string().required().min(3, "Should be 3 chars minimum"),
+	title: Yup.string()
+		.required()
+		.min(3, "Should be 3 chars minimum")
+		.max(255, "Max of 255 characters"),
 });
