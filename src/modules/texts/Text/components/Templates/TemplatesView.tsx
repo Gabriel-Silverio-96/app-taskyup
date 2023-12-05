11d284f08a8a140778d524fad80bb8b75aee2b5c
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-12-05 17:14:41 -0300

    style(texts): code style

--- src/modules/texts/Text/components/Templates/TemplatesView.tsx
+++ src/modules/texts/Text/components/Templates/TemplatesView.tsx
@@ -14,6 +14,7 @@ import CardTemplate from "./components/CardTemplate";
 
 const TemplatesView: React.FC<ITemplatesView> = props => {
 	const { isOpenTemplates, mutate, isLoading } = props;
+
 	return (
 		<TemplateContainer open={isOpenTemplates}>
 			<Loading isLoading={isLoading} backdrop message="Creating text" />
