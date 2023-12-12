55c2005ee29a53562f79dbb32611baccc2d3ce04
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-12-12 17:54:13 -0300

    refactor(texts): remove interfaces

--- src/modules/texts/TextEdit/components/Markdown/types/Markdown.types.ts
+++ src/modules/texts/TextEdit/components/Markdown/types/Markdown.types.ts
@@ -1,7 +1,6 @@
-import { ChangeEvent } from "react";
-import { IFetchResponseDefault } from "shared/common/types/Fetch";
-import { FunctionReturnsVoid } from "shared/common/types/AppTypes";
 import { IDataText } from "modules/texts/TextEdit/types";
+import { ChangeEvent } from "react";
+import { FunctionReturnsVoid } from "shared/common/types/AppTypes";
 
 export interface IMarkdownView {
 	dataText: IDataText;
@@ -10,26 +9,4 @@ export interface IMarkdownView {
 	onChangeTextTitle: (event: ChangeEvent<HTMLInputElement>) => void;
 	isLoading: boolean;
 	isSaving: boolean;
-}
-
-export interface IFetchGetOneTextService {
-	(text_id: string | null): Promise<IFetchGetOneTextResponse>;
-}
-
-export interface IFetchPatchTextPayload {
-	board_id: string | null;
-	text_id: string | null;
-	data: IFetchGetOneTextResponse;
-}
-
-export interface IFetchPatchTextService {
-	(payload: IFetchPatchTextPayload): Promise<IFetchResponseDefault>;
-}
-
-export interface IFetchGetOneTextResponse {
-	title_board: string;
-	title_text: string;
-	text: string;
-	created_at: string;
-	public: boolean;
 }
