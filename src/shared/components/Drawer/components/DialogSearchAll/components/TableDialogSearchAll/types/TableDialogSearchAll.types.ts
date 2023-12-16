4fab4041dabec81d429d655900ae68842760ab30
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-12-16 17:28:39 -0300

    refactor(shared): interfaces

--- src/shared/components/Drawer/components/DialogSearchAll/components/TableDialogSearchAll/types/TableDialogSearchAll.types.ts
+++ src/shared/components/Drawer/components/DialogSearchAll/components/TableDialogSearchAll/types/TableDialogSearchAll.types.ts
@@ -1,17 +1,18 @@
 import { IPaginationModel } from "shared/common/types/AppTypes";
-import { IFetchGetSearchAllResponse } from "shared/components/Drawer/components/DialogSearchAll/types";
 import { Dispatch, SetStateAction } from "react";
 import { GridColDef, GridRowsProp } from "@mui/x-data-grid";
+import { IFetchGetSearchAllResponse } from "shared/components/Drawer/components/DialogSearchAll/service/types";
 
 export interface ITableDialogSearchAll {
-    data: IFetchGetSearchAllResponse | undefined;
-    isLoading: boolean;
-    paginationModel: IPaginationModel;
-    setPaginationModel: Dispatch<SetStateAction<IPaginationModel>>;
+	data: IFetchGetSearchAllResponse | undefined;
+	isLoading: boolean;
+	paginationModel: IPaginationModel;
+	setPaginationModel: Dispatch<SetStateAction<IPaginationModel>>;
 }
 
-export interface ITableDialogSearchAllView extends Omit<ITableDialogSearchAll, "setPaginationModel"> {
-    rows: GridRowsProp;
-    columns: GridColDef[];
-    hadlePaginationModelChange: (props: IPaginationModel) => void;
+export interface ITableDialogSearchAllView
+	extends Omit<ITableDialogSearchAll, "setPaginationModel"> {
+	rows: GridRowsProp;
+	columns: GridColDef[];
+	hadlePaginationModelChange: (props: IPaginationModel) => void;
 }
--- src/shared/components/Drawer/components/DialogSearchAll/types/DialogSearchAll.types.ts
+++ src/shared/components/Drawer/components/DialogSearchAll/types/DialogSearchAll.types.ts
@@ -7,31 +7,10 @@ import {
 import { IDialogSearchAllState } from "shared/common/store/DialogSearchAll/types/DialogSearchAll.types";
 import { IPaginationModel } from "shared/common/types/AppTypes";
 import { FunctionReturnsVoid } from "shared/common/types/AppTypes";
+import { IFetchGetSearchAllResponse } from "shared/components/Drawer/components/DialogSearchAll/service/types";
 
 export interface IDialogSearchAllForm {
 	query: string;
-}
-
-export interface IFetchGetSearchAllService {
-	query: string;
-	pageSize?: number;
-	pageNumber?: number;
-}
-
-export interface IFetchGetSearchAllResponseResults {
-	id: string;
-	title: string;
-	title_board: string;
-	board_id: string;
-	type_board: string;
-}
-
-export interface IFetchGetSearchAllResponse {
-	pageSize: number;
-	pageNumber: number;
-	offset: number;
-	countResult: number;
-	results: Array<IFetchGetSearchAllResponseResults>;
 }
 
 export interface IDialogSearchAllView {
