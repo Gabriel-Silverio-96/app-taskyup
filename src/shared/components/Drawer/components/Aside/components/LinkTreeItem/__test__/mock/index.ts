c8eceb4101d4b5420547f8d071c65acb52e88fc1
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2023-12-19 18:31:29 -0300

    feat(shared): add items mock

--- src/shared/components/Drawer/components/Aside/components/LinkTreeItem/__test__/mock/index.ts
+++ src/shared/components/Drawer/components/Aside/components/LinkTreeItem/__test__/mock/index.ts
@@ -6,21 +6,25 @@ export const MENU_MOCK = {
 			board_id: faker.datatype.uuid(),
 			title: faker.lorem.word(5),
 			board_type_id: "notes",
-			items: {
-				board_id: faker.datatype.uuid(),
-				item_id: faker.datatype.uuid(),
-				title: faker.lorem.word(5),
-			},
+			items: [
+				{
+					board_id: faker.datatype.uuid(),
+					item_id: faker.datatype.uuid(),
+					title: faker.lorem.word(5),
+				},
+			],
 		},
 		{
 			board_id: faker.datatype.uuid(),
 			title: faker.lorem.word(5),
 			board_type_id: "notes",
-			items: {
-				board_id: faker.datatype.uuid(),
-				item_id: faker.datatype.uuid(),
-				title: faker.lorem.word(5),
-			},
+			items: [
+				{
+					board_id: faker.datatype.uuid(),
+					item_id: faker.datatype.uuid(),
+					title: faker.lorem.word(5),
+				},
+			],
 		},
 	],
 	texts: [
@@ -28,21 +32,25 @@ export const MENU_MOCK = {
 			board_id: faker.datatype.uuid(),
 			title: faker.lorem.word(5),
 			board_type_id: "texts",
-			items: {
-				board_id: faker.datatype.uuid(),
-				item_id: faker.datatype.uuid(),
-				title: faker.lorem.word(5),
-			},
+			items: [
+				{
+					board_id: faker.datatype.uuid(),
+					item_id: faker.datatype.uuid(),
+					title: faker.lorem.word(5),
+				},
+			],
 		},
 		{
 			board_id: faker.datatype.uuid(),
 			title: faker.lorem.word(5),
 			board_type_id: "texts",
-			items: {
-				board_id: faker.datatype.uuid(),
-				item_id: faker.datatype.uuid(),
-				title: faker.lorem.word(5),
-			},
+			items: [
+				{
+					board_id: faker.datatype.uuid(),
+					item_id: faker.datatype.uuid(),
+					title: faker.lorem.word(5),
+				},
+			],
 		},
 	],
 };
