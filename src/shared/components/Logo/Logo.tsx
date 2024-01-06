1c0439bea6363a573d424f9915e827d320445987
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2024-01-06 08:14:59 -0300

    refactor(shared): theme magic string

--- src/shared/components/Logo/Logo.tsx
+++ src/shared/components/Logo/Logo.tsx
@@ -2,11 +2,12 @@ import { useThemeContext } from "shared/common/Context/ThemeContext";
 import React, { memo } from "react";
 import LogoView from "./LogoView";
 import { ILogo } from "./types/Logo.component";
+import { THEME_NAME } from "shared/constants";
 
 const Logo: React.FC<ILogo> = ({ size = 143.146, isoType }) => {
 	const { themeName, theme } = useThemeContext();
 	const logoColor =
-		themeName === "dark"
+		themeName === THEME_NAME.DARK
 			? theme.palette.common.white
 			: theme.palette.common.black;
 
