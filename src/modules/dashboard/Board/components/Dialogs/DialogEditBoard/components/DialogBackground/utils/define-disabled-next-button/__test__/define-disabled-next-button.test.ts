ba4f6cd33a0119510062a3e1237182afacfa4cdb
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2024-01-10 19:41:43 -0300

    feat(dashboard): create util to define disabled next button in pagination

---
+++ src/modules/dashboard/Board/components/Dialogs/DialogEditBoard/components/DialogBackground/utils/define-disabled-next-button/__test__/define-disabled-next-button.test.ts
@@ -0,0 +1,55 @@
+import { faker } from "@faker-js/faker";
+import { defineDisabledNextButton } from "../define-disabled-next-button";
+
+const PHOTHOS_MOCK = {
+	id: faker.datatype.number(),
+	photographer: faker.name.fullName(),
+	photographer_url: faker.internet.url(),
+	src: {
+		tiny: faker.image.imageUrl(),
+	},
+	alt: faker.lorem.lines(),
+};
+
+describe("Funtion defineDisabledNextButton()", () => {
+	it("Should return true when pagination is equal to or greater than total pages", () => {
+		const images = {
+			total_results: 30,
+			per_page: 10,
+			photos: [],
+		};
+		const pagination = 3;
+
+		const result = defineDisabledNextButton({ images, pagination });
+
+		expect(result).toBe(true);
+	});
+
+	it("Should return true when the number of photos is less than 9", () => {
+		const images = {
+			total_results: 30,
+			per_page: 10,
+			photos: [],
+		};
+		const pagination = 1;
+
+		const result = defineDisabledNextButton({ images, pagination });
+
+		expect(result).toBe(true);
+	});
+
+	it("Should return false when pagination is less than total pages and the number of photos is 9 or more", () => {
+		const photos = Array.from({ length: 9 }, () => PHOTHOS_MOCK); // Mock 9 photos
+
+		const images = {
+			total_results: 30,
+			per_page: 10,
+			photos,
+		};
+		const pagination = 1;
+
+		const result = defineDisabledNextButton({ images, pagination });
+
+		expect(result).toBe(false);
+	});
+});
---
+++ src/modules/dashboard/Board/components/Dialogs/DialogEditBoard/components/DialogBackground/utils/define-disabled-next-button/define-disabled-next-button.ts
@@ -0,0 +1,26 @@
+import { IImages } from "modules/dashboard/Board/components/Dialogs/DialogEditBoard/components/DialogBackground/types";
+
+/**
+ * Determines whether the "Next" button should be disabled based on the current pagination state.
+ *
+ * @param {Object} options - The options object.
+ * @param {Object} options.images - The images data, including total results, per page, and an array of photos.
+ * @param {number} options.pagination - The current pagination value.
+ * @returns {boolean} - Returns `true` if the "Next" button should be disabled, otherwise `false`.
+ */
+export const defineDisabledNextButton = ({
+	images,
+	pagination,
+}: {
+	images: IImages;
+	pagination: number;
+}): boolean => {
+	const totalResults = images.total_results;
+	const perPage = images.per_page;
+	const totalPages = Math.ceil(totalResults / perPage);
+
+	const isDisabledNextButton =
+		pagination >= totalPages || images.photos.length < 9;
+
+	return isDisabledNextButton;
+};
