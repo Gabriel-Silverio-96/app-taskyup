c1ceca4a985446448bd874b34f753b9ee2c74516
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2024-03-30 20:07:06 -0300

    feat: define icon color

--- src/modules/notes/Note/components/CardNote/components/CardAction/CardAction.tsx
+++ src/modules/notes/Note/components/CardNote/components/CardAction/CardAction.tsx
@@ -3,11 +3,16 @@ import { useDialogNote } from "modules/notes/Note/shared/hook/useDialogNote";
 import { ICON_SIZE } from "shared/constants";
 import { createURLQueryParams } from "shared/util/createURLQueryParams";
 import { CardActionContainer } from "modules/notes/Note/components/CardNote/components/CardAction/style";
-import { FiEye, FiTrash } from "react-icons/fi";
+import { FiEye, FiStar, FiTrash } from "react-icons/fi";
 import { Link } from "react-router-dom";
 import { ICardAction } from "modules/notes/Note/components/CardNote/components/CardAction/types";
 
-const CardAction: React.FC<ICardAction> = ({ note_id, board_id }) => {
+const CardAction: React.FC<ICardAction> = ({
+	note_id,
+	board_id,
+	favorite_id,
+	favorite,
+}) => {
 	const { palette } = useTheme();
 
 	const { openDialogDeleteOneNote } = useDialogNote();
@@ -17,10 +22,17 @@ const CardAction: React.FC<ICardAction> = ({ note_id, board_id }) => {
 		board_id,
 	});
 
+	const defineIconColor = favorite
+		? palette.secondary.main
+		: palette.common.white;
+
 	return (
 		<CardActionContainer id="card-action">
 			<IconButton onClick={() => openDialogDeleteOneNote(note_id)}>
 				<FiTrash color={palette.error.main} size={ICON_SIZE.MEDIUM} />
+			</IconButton>
+			<IconButton>
+				<FiStar color={defineIconColor} size={ICON_SIZE.MEDIUM} />
 			</IconButton>
 			<Link to={linkToNoteEdit}>
 				<IconButton>
