b1d7b2b04e574a10ed3296d7275d0cf87552d88a
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2024-04-21 14:24:54 -0300

    test: create unit test to function

---
+++ src/shared/util/fieldErrors/__test__/fieldErrors.test.ts
@@ -0,0 +1,35 @@
+import { faker } from "@faker-js/faker";
+import { fieldErrors } from "../fieldErrors";
+
+describe("Function fieldErrors()", () => {
+	it("Should return error false and helperText empty string when error is falsy", () => {
+		const errors = undefined;
+		const field = "name";
+
+		const result = fieldErrors({ errors, field });
+		const expected = { error: false, helperText: "" };
+
+		expect(result).toEqual(expected);
+	});
+
+	it("Should return error true and helperText message when error exists for the field", () => {
+		const message = faker.lorem.paragraph();
+		const errors = { name: { message } };
+		const field = "name";
+
+		const result = fieldErrors({ errors, field });
+		const expected = { error: true, helperText: message };
+
+		expect(result).toEqual(expected);
+	});
+
+	it("Should return error false and empty helperText when no error exists for the field", () => {
+		const errors = {};
+		const field = "name";
+
+		const result = fieldErrors({ errors, field });
+		const expected = { error: false, helperText: "" };
+
+		expect(result).toEqual(expected);
+	});
+});
