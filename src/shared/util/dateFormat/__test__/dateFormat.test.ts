06dbbaf89262ae45bd549be15623e1db1d6d490c
Gabriel Silvério <53228013+Gabriel-Silverio-96@users.noreply.github.com>
2024-05-10 20:01:00 -0300

    test: util dateFormat (#196)
    * test: create unit test
    
    * refactor: error message

---
+++ src/shared/util/dateFormat/__test__/dateFormat.test.ts
@@ -0,0 +1,29 @@
+import { dateFormat, errorMessage } from "shared/util/dateFormat";
+
+describe("Function dateFormat()", () => {
+	test("should format the date string into a localized date string", () => {
+		const date = "2024-04-20T00:00";
+		const locale = "en-US";
+
+		const result = dateFormat(date, locale);
+
+		const expected = "4/20/2024";
+		expect(result).toEqual(expected);
+	});
+
+	test("should use the default locale 'en-US' if no locale is provided", () => {
+		const date = "2024-04-20T00:00";
+
+		const result = dateFormat(date);
+
+		const expected = "4/20/2024";
+		expect(result).toEqual(expected);
+	});
+
+	test("should throw an error if the date  is 'falsy'", () => {
+		const date = null;
+		const act = () => dateFormat(date as any);
+
+		expect(act).toThrowError(errorMessage);
+	});
+});
