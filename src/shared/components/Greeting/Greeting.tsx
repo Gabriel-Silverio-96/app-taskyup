9f8e3ba32a1b73dd4bf1348774891f4d60ec386d
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2024-05-25 17:25:49 -0300

    refactor: delete GreetingView

--- src/shared/components/Greeting/Greeting.tsx
+++ src/shared/components/Greeting/Greeting.tsx
@@ -1,9 +1,23 @@
+import { Typography } from "@mui/material";
 import React, { memo } from "react";
-import GreetingView from "./GreetingView";
+import Logo from "../Logo";
+import { GreetingMessage } from "./styles";
 import { IGreeting } from "./types/Greeting.component";
 
 const Greeting: React.FC<IGreeting> = ({ title, subtitle }) => {
-	return <GreetingView {...{ title, subtitle }} />;
+	return (
+		<div>
+			<Logo size={125} />
+			<GreetingMessage>
+				<Typography variant="h4" fontWeight={700}>
+					{title}
+				</Typography>
+				<Typography variant="body1" color="GrayText">
+					{subtitle}
+				</Typography>
+			</GreetingMessage>
+		</div>
+	);
 };
 
 export default memo(Greeting);
