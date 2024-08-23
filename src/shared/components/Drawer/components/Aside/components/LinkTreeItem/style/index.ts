9726c1b0c2ec0b1d5549fa02f055199c4ad9d6a2
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2024-08-23 12:50:07 -0300

    refactor: use theme

--- src/shared/components/Drawer/components/Aside/components/LinkTreeItem/style/index.ts
+++ src/shared/components/Drawer/components/Aside/components/LinkTreeItem/style/index.ts
@@ -15,17 +15,17 @@ export const TreeItemContainer = styled("div")(
 );
 
 export const LinkIcon = styled(Link)(
-	() => `                
-        margin-top: 10px;
-        padding: 5px;
+	({ theme }) => `                
+        margin-top: ${theme.spacing(1.25)};
+        padding: ${theme.spacing(0.625)};
         opacity: 0;
         transition: opacity .3s ease-out;
     `
 );
 
 export const TreeItemLabel = styled("div")(
-	() => `                
-        width: 76px;
+	({ theme }) => `                
+        width: ${theme.spacing(9.5)};
         white-space: nowrap;
         overflow: hidden;
         text-overflow: ellipsis;
