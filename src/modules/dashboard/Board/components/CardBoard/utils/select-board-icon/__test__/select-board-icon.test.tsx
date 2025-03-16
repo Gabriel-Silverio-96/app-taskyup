6e4446388c8a9f9df28f4b554c3946f811261e34
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2025-03-16 20:43:39 -0300

    refactor: variable name

---
+++ src/modules/dashboard/Board/components/CardBoard/utils/select-board-icon/__test__/select-board-icon.test.tsx
@@ -0,0 +1,43 @@
+import { faker } from "@faker-js/faker";
+import { selectBoardIcon } from "modules/dashboard/Board/components/CardBoard/utils/select-board-icon";
+import { GoNote } from "react-icons/go";
+import { MdOutlineNotes, MdOutlineSpaceDashboard } from "react-icons/md";
+import { ICON_SIZE } from "shared/constants";
+
+describe("Function selectBoardIcon()", () => {
+	test("Should return the correct icon for notes", () => {
+		const iconColor = faker.color.human();
+		const typeBoard = "notes";
+		const expectedIcon = (
+			<GoNote size={ICON_SIZE.MEDIUM} color={iconColor} />
+		);
+
+		const result = selectBoardIcon(typeBoard, iconColor);
+		expect(result).toEqual(expectedIcon);
+	});
+
+	test("Should return the correct icon for texts", () => {
+		const iconColor = faker.color.human();
+		const typeBoard = "texts";
+		const expectedIcon = (
+			<MdOutlineNotes size={ICON_SIZE.MEDIUM} color={iconColor} />
+		);
+
+		const result = selectBoardIcon(typeBoard, iconColor);
+		expect(result).toEqual(expectedIcon);
+	});
+
+	test("Should return the correct icon for kanban", () => {
+		const iconColor = faker.color.human();
+		const typeBoard = "kanban";
+		const expectedIcon = (
+			<MdOutlineSpaceDashboard
+				size={ICON_SIZE.MEDIUM}
+				color={iconColor}
+			/>
+		);
+
+		const result = selectBoardIcon(typeBoard, iconColor);
+		expect(result).toEqual(expectedIcon);
+	});
+});
--- src/modules/dashboard/Board/components/CardBoard/utils/select-board-icon/select-board-icon.tsx
+++ src/modules/dashboard/Board/components/CardBoard/utils/select-board-icon/select-board-icon.tsx
@@ -4,12 +4,12 @@ import { MdOutlineNotes, MdOutlineSpaceDashboard } from "react-icons/md";
 import { TypeBoard } from "shared/common/types";
 import { ICON_SIZE } from "shared/constants";
 
-interface IBoardIcon {
+interface IIconMapping {
 	[key: string]: ReactNode;
 }
 
 const selectBoardIcon = (typeBoard: TypeBoard, iconColor: string) => {
-	const boardIcon: IBoardIcon = {
+	const iconMapping: IIconMapping = {
 		notes: <GoNote size={ICON_SIZE.MEDIUM} color={iconColor} />,
 		texts: <MdOutlineNotes size={ICON_SIZE.MEDIUM} color={iconColor} />,
 		kanban: (
@@ -20,7 +20,7 @@ const selectBoardIcon = (typeBoard: TypeBoard, iconColor: string) => {
 		),
 	};
 
-	return boardIcon[typeBoard];
+	return iconMapping[typeBoard];
 };
 
 export { selectBoardIcon };
