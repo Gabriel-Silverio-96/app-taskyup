585f3e99f21d82feaa32f2b8fa7594da115046df
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2025-03-16 08:07:35 -0300

    refactor: removed unused style

---
+++ src/modules/auth/SignIn/sign-in.style.ts
@@ -0,0 +1,14 @@
+import { styled } from "@mui/material";
+
+export const Footer = styled("footer")(
+	({ theme }) => `
+        a {
+            text-decoration: none;
+            color: ${theme.palette.grey[700]};                   
+        }
+
+        hr {
+            width: 50%
+        }s
+    `
+);
