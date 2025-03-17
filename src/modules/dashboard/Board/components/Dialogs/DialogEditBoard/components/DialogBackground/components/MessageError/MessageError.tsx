7fc58a3a989d6e4ef3388a758f0a555efec106fb
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2025-03-17 18:54:00 -0300

    refactor: use absolute

--- src/modules/dashboard/Board/components/Dialogs/DialogEditBoard/components/DialogBackground/components/MessageError/MessageError.tsx
+++ src/modules/dashboard/Board/components/Dialogs/DialogEditBoard/components/DialogBackground/components/MessageError/MessageError.tsx
@@ -1,6 +1,6 @@
 import { Typography } from "@mui/material";
 import React from "react";
-import { IMessageError } from "./types";
+import { IMessageError } from "modules/dashboard/Board/components/Dialogs/DialogEditBoard/components/DialogBackground/components/MessageError/types";
 
 const MessageError: React.FC<IMessageError> = ({ images }) => {
 	const isHidden = Boolean(images.error);
