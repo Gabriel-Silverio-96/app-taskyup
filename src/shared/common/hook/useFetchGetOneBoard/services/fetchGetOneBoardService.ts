a6ab728b49514a14b210f887829ad08ebea71f6a
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2025-03-19 13:45:04 -0300

    refactor: remove unnecessary custom hook useFetchGetOneBoard

--- src/shared/common/hook/useFetchGetOneBoard/services/fetchGetOneBoardService.ts
+++
@@ -1,9 +0,0 @@
-import api from "shared/services/api";
-import { IFetchGetOneBoardResponse } from "shared/common/hook/useFetchGetOneBoard/types";
-
-export const fetchGetOneBoardService = async (boardID: string | undefined) => {
-	const { data } = await api.get<IFetchGetOneBoardResponse>(
-		`/board/board_id=${boardID}`
-	);
-	return data;
-};
