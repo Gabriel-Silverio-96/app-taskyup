0a709fc02fbe8c97fce2cb68888a111639bb3c98
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2025-03-21 15:15:04 -0300

    refactor: rename the file format to snack_case

--- src/modules/error/Page404/Page404.tsx
+++ src/modules/error/Page404/Page404.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import Page404View from "./Page404View";
+import Page404View from "modules/error/Page404/Page404View";
 
 const Page404: React.FC = () => {
-	const navigate = useNavigate();    
+	const navigate = useNavigate();
 	const backPage = () => navigate(-1);
 
-	return <Page404View {...{ backPage }}/>;
+	return <Page404View {...{ backPage }} />;
 };
 
 export default Page404;
--- src/modules/error/Page404/Page404View.tsx
+++ src/modules/error/Page404/Page404View.tsx
@@ -1,23 +1,26 @@
 import { Button, Grid, Typography } from "@mui/material";
 import React from "react";
-import { IPage404View } from "./types/Page404.component";
+import type { IPage404View } from "modules/error/Page404/types";
 
-const Page404View: React.FC<IPage404View> = (props) => {
+const Page404View: React.FC<IPage404View> = props => {
 	const { backPage } = props;
 	return (
-		<Grid 
-			container 
-			justifyContent="center" 
-			alignItems="center" 
-			sx={ { height: "100vh" }}
-			textAlign="center"
-		>
+		<Grid
+			container
+			justifyContent="center"
+			alignItems="center"
+			sx={{ height: "100vh" }}
+			textAlign="center">
 			<Grid item md={12}>
-				<Typography variant="h1" fontWeight={700}>404</Typography>
+				<Typography variant="h1" fontWeight={700}>
+					404
+				</Typography>
 				<Typography variant="body2" color="GrayText">
 					The page you are trying to access does not exist
 				</Typography>
-			    <Button sx={{ mt: 3 }} onClick={backPage}>Back page</Button>
+				<Button sx={{ mt: 3 }} onClick={backPage}>
+					Back page
+				</Button>
 			</Grid>
 		</Grid>
 	);
