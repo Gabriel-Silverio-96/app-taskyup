0f52618496fa16760f149b137d817e19c86df918
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2025-03-22 11:21:41 -0300

    refactor: remove unnecessary files

--- src/modules/notes/Note/components/HeaderNote/constants/HeaderNote.constants.ts
+++
@@ -1,5 +0,0 @@
-export const FORM_CREATE_NOTE = {
-	color_note: "#9C10FF",
-	title_note: "Title note",
-	observation: "",
-};
--- src/modules/notes/Note/components/HeaderNote/constants/index.ts
+++
@@ -1,2 +0,0 @@
-export * as default from "./HeaderNote.constants";
-export * from "./HeaderNote.constants";
