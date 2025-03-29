b8dd664e53548aef6976ed1ff113353110101d70
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2025-03-29 16:00:08 -0300

    refactor: rename the file format to kebab-case

--- src/shared/components/Drawer/components/DialogSearchAll/components/TableDialogSearchAll/utils/generateQueryParams/generateQueryParams.ts
+++ src/shared/components/Drawer/components/DialogSearchAll/components/TableDialogSearchAll/utils/generateQueryParams/generateQueryParams.ts
@@ -1,6 +1,13 @@
-import { IGenerateQueryParams, IGenerateQueryParamsReturn } from "./types/generateQueryParams.types";
+import type {
+	IGenerateQueryParams,
+	IGenerateQueryParamsReturn,
+} from "shared/components/Drawer/components/DialogSearchAll/components/TableDialogSearchAll/utils/generateQueryParams/types";
 
-const generateQueryParams = ({ type_board,	board_id, id }: IGenerateQueryParams): IGenerateQueryParamsReturn => {
+const generateQueryParams = ({
+	type_board,
+	board_id,
+	id,
+}: IGenerateQueryParams): IGenerateQueryParamsReturn => {
 	const params = type_board === "texts" ? { text_id: id } : { note_id: id };
 	return { board_id, ...params };
 };
---
+++ src/shared/components/Drawer/components/DialogSearchAll/components/TableDialogSearchAll/utils/generateQueryParams/types/index.ts
@@ -0,0 +1,1 @@
+export * from "./generate-query-params.types";
