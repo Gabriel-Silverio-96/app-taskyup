36d8541a1597d1c23b3dcf392f6002e536cd652f
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2025-03-30 15:46:22 -0300

    refactor: create style file

--- src/shared/components/RequiredAuth/required-auth.style.ts
+++ src/shared/components/RequiredAuth/required-auth.style.ts
@@ -1,10 +1,10 @@
 import { styled, Box as MuiBox } from "@mui/material";
 
 export const Box = styled(MuiBox)(
-	({theme}) => `
+	({ theme }) => `
         flex-grow: 1;
         overflow: auto;
         height: calc(100vh - 67px);
         padding: ${theme.spacing(3)} ${theme.spacing(3)} ${theme.spacing(16)};
-    `    
+    `
 );
