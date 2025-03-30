900aeab4faf0d0ac0129191626c5f4cf931c50c2
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2025-03-30 16:06:23 -0300

    refactor: export all utils

---
+++ src/shared/utils/index.ts
@@ -0,0 +1,5 @@
+export * from "./validate-email-domain-length";
+export * from "./field-errors";
+export * from "./define-value-created-at";
+export * from "./date-format";
+export * from "./create-url-query-params";
