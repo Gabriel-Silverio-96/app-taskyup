38c2277dd6516314e2a915c4a437e774304ad09c
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2025-04-10 18:42:43 -0300

    fix: size title texts

--- src/modules/texts/Text/components/HeaderText/HeaderTextView.tsx
+++ src/modules/texts/Text/components/HeaderText/HeaderTextView.tsx
@@ -1,10 +1,11 @@
-import { Grid, IconButton, Tooltip, Typography } from "@mui/material";
+import { Grid, IconButton, Tooltip } from "@mui/material";
 import React from "react";
 import { FiTrash, FiUsers } from "react-icons/fi";
 import { MdOutlineLightbulb } from "react-icons/md";
 import { Nav } from "modules/texts/Text/components/HeaderText/header-text.style";
 import type { IHeaderTextView } from "modules/texts/Text/components/HeaderText/types";
 import { ICON_SIZE } from "shared/constants";
+import { TypographyTitle } from "modules/notes/Note/components/HeaderNote/header-note.style";
 
 const HeaderTextView: React.FC<IHeaderTextView> = props => {
 	const {
@@ -18,9 +19,11 @@ const HeaderTextView: React.FC<IHeaderTextView> = props => {
 	return (
 		<Grid container alignItems="center" sx={{ mb: 3 }}>
 			<Grid item xl={6} md={6} sm={8} xs={8}>
-				<Typography variant="h6" fontWeight={800}>
-					{title}
-				</Typography>
+				<Tooltip arrow title={title || ""}>
+					<TypographyTitle variant="h6" fontWeight={800}>
+						{title}
+					</TypographyTitle>
+				</Tooltip>
 			</Grid>
 			<Grid item xl={6} md={6} sm={4} xs={4}>
 				<Nav>
