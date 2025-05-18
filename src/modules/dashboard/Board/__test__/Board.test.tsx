755bcd93f4a2889e9f961bf3376e81b4df07ddd9
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2025-05-18 14:20:41 -0300

    test: improve test coverage delete board

--- src/modules/dashboard/Board/__test__/Board.test.tsx
+++ src/modules/dashboard/Board/__test__/Board.test.tsx
@@ -10,6 +10,8 @@ import {
 	EDIT_BOARD_DATA_MOCK,
 	LIST_BOARD_MOCK_SUCCESS_RESPONSE_MOCK,
 	EDIT_BOARD_SUCCESS_RESPONSE_MOCK,
+	DELETE_BOARD_DATA_MOCK,
+	DELETE_BOARD_SUCCESS_RESPONSE_MOCK,
 } from "./mock";
 
 const mock = new MockAdapter(api);
@@ -77,28 +79,67 @@ describe("Component <Board />", () => {
 	test("Edit board", async () => {
 		mock.onGet("board").reply(200, LIST_BOARD_MOCK_SUCCESS_RESPONSE_MOCK);
 		renderRequiredAuth(<Board />);
-	
+
 		const urlGetBoard = `board/board_id=${EDIT_BOARD_DATA_MOCK.board_id}`;
 		mock.onGet(urlGetBoard).reply(200, EDIT_BOARD_DATA_MOCK);
-	
+
 		const urlPatchBoard = `board/edit/board_id=${EDIT_BOARD_DATA_MOCK.board_id}`;
-		mock.onPatch(urlPatchBoard).reply(201, EDIT_BOARD_SUCCESS_RESPONSE_MOCK);
-	
-		const buttonCardBoardOption = await screen.findAllByTestId("button-card-board-options");
+		mock.onPatch(urlPatchBoard).reply(
+			201,
+			EDIT_BOARD_SUCCESS_RESPONSE_MOCK
+		);
+
+		const buttonCardBoardOption = await screen.findAllByTestId(
+			"button-card-board-options"
+		);
 		userEvent.click(buttonCardBoardOption[0]);
-	
+
 		const optionEditBoard = await screen.findByText("Edit");
 		userEvent.click(optionEditBoard);
-	
-		const inputBoardName = await screen.findByTestId<HTMLInputElement>("input-edit");
-		 userEvent.clear(inputBoardName);
-		 userEvent.type(inputBoardName, board_name_edited);
-	
+
+		const inputBoardName = await screen.findByTestId<HTMLInputElement>(
+			"input-edit"
+		);
+		userEvent.clear(inputBoardName);
+		userEvent.type(inputBoardName, board_name_edited);
+
 		await waitFor(async () => {
 			const buttonSave = screen.getByRole("button", { name: "Save" });
 			userEvent.click(buttonSave);
+
 			const snackbarMessage = await screen.findByText(
 				EDIT_BOARD_SUCCESS_RESPONSE_MOCK.message
+			);
+			expect(snackbarMessage).toBeInTheDocument();
+		});
+	});
+
+	test("Delete board", async () => {
+		mock.onGet("board").reply(200, LIST_BOARD_MOCK_SUCCESS_RESPONSE_MOCK);
+		renderRequiredAuth(<Board />);
+
+		const urlDetBoard = `board/delete/board_id=${DELETE_BOARD_DATA_MOCK.board_id}`;
+		mock.onDelete(urlDetBoard).reply(
+			200,
+			DELETE_BOARD_SUCCESS_RESPONSE_MOCK
+		);
+
+		const buttonCardBoardOption = await screen.findAllByTestId(
+			"button-card-board-options"
+		);
+		userEvent.click(buttonCardBoardOption[0]);
+
+		const optionDeleteBoard = await screen.findByText("Delete");
+		userEvent.click(optionDeleteBoard);
+
+		await waitFor(async () => {
+			const buttonConfirm = screen.getByRole<HTMLInputElement>("button", {
+				name: "Yes",
+			});
+			userEvent.click(buttonConfirm);
+
+			const snackbarMessage = await screen.findByText(
+				DELETE_BOARD_SUCCESS_RESPONSE_MOCK.message
 			);
 			expect(snackbarMessage).toBeInTheDocument();
 		});
